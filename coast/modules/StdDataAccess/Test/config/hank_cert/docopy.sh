4366e6eb610235ec236ad98c7326f6a9e20abcab
Markus Pfister <pfm@bluewin.ch>
2005-01-24 18:02:54 +0000

    added copy file

---
+++ coast/modules/StdDataAccess/Test/config/hank_cert/docopy.sh
@@ -0,0 +1,5 @@
+#!/bin/ksh
+cp /tmp/mkca/ca-hierarchy/ucerts/clientcrt_dylan.hank.com.pem clientcrt.pem
+cp /tmp/mkca/ca-hierarchy/ucerts/clientkey_dylan.hank.com.pem clientkey.pem
+cp /tmp/mkca/ca-hierarchy/ucerts/rootCA1crt.pem .
+/* empty file */
