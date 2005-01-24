7471bab3f34f73f6c82a5d6d5db2a9f2ae29fca2
Markus Pfister <pfm@bluewin.ch>
2005-01-24 15:13:48 +0000

    added Hank cert

---
+++ coast/modules/SSL/Test/config/hank_cert/docopy.sh
@@ -0,0 +1,8 @@
+#!/bin/ksh
+cp clientcrt_bondo.pem clientcrt.pem
+cp clientkey_bondo.pem clientkey.pem
+cp servercrt_dylan.hank.com.pem servercrt.pem
+cp serverkey_dylan.hank.com.pem serverkey.pem
+cp fullchain_dylan.hank.com.pem fullchain.pem
+cp serverchain_dylan.hank.com.pem serverchain.pem
+
