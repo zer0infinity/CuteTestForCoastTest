bd4072e0cd02cdd16c8ef0655cb1dbf50e2cced9
Markus Pfister <pfm@bluewin.ch>
2005-02-14 16:20:06 +0000

    ROAnyConfNamedObjectLookupAdapter looks into a ConfiguredNamedObject's config if the looked up   slot has not been found in the loooked-up Anything the ROAnythingLookupAdapter works on.

--- coast/wdbase/ROAnyLookupAdapter.h
+++ coast/wdbase/ROAnyLookupAdapter.h
@@ -10,6 +10,7 @@
 #define _ROAnyLookupAdapter_H
 
 #include "LookupInterface.h"
+#include "IFAConfObject.h"
 
 //---- ROAnyLookupAdapter ----------------------------------------------------------
 //! <B>Wraps ROAnything in a LookupInterface (as needed eg. by Context::Push())</B>
@@ -26,4 +27,26 @@ protected:
 	}
 };
 
+class ROAnyConfNamedObjectLookupAdapter : public ROAnyLookupAdapter
+{
+public:
+	ROAnyConfNamedObjectLookupAdapter(ROAnything ro, ConfNamedObject *cfno): ROAnyLookupAdapter(ro) {
+		fCfno = cfno;
+	}
+	~ROAnyConfNamedObjectLookupAdapter() {}
+protected:
+	ConfNamedObject *fCfno;
+
+	virtual bool DoLookup(const char *key, ROAnything &result, char delim, char indexdelim) const {
+		if ( !ROAnyLookupAdapter::DoLookup(key, result, delim, indexdelim) ) {
+			if ( fCfno != (ConfNamedObject *) NULL ) {
+				return fCfno->Lookup(key, result, delim, indexdelim);
+			} else {
+				return false;
+			}
+		}
+		return true;
+
+	}
+};
 #endif
