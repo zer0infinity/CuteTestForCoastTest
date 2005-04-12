5b896dc77443d1a0b7e7f182f9ed8af30f562264
Markus Pfister <pfm@bluewin.ch>
2005-04-12 10:50:57 +0000

    Return false to HTTPProcessor::DoReadInput in *all* cases where RequestReader detected an error.   Current code version handled the error (logging and HTML-rendering) but returned "true". As a consequence,   the request was passed on to the framework. This created nasty side-effects, e.g:   - Request is faulty, RequestReader generates correct error reply   - Request is passed to the framework. Cookies are not  present, because RequestReader stopped reading after     the first erranous request header field.   - Because there are no cookies in the request, the login page is rendered and displayed by the browser. (Instead     of showding the expected error page. Added / refactored testcases

--- coast/modules/StdDataAccess/Test/HTTPProcessorTest.cpp
+++ coast/modules/StdDataAccess/Test/HTTPProcessorTest.cpp
@@ -8,6 +8,7 @@
 
 //--- standard modules used ----------------------------------------------------
 #include "StringStream.h"
+#include "StringStreamSocket.h"
 #include "System.h"
 #include "Context.h"
 #include "URLUtils.h"
@@ -21,6 +22,11 @@
 
 //--- interface include --------------------------------------------------------
 #include "HTTPProcessorTest.h"
+
+static String common =
+	String("") << "</head><body bgcolor=\"silver\">\n"
+	<< "<center>\n<h1>Page not found.</h1>\nPress the back button to return to the previous page!<br><br>\n"
+	<< "<FORM><input type=button value=\"Back\" onClick=\"javascript:history.back(1)\"></FORM>\n</center>\n</body></html>";
 
 //---- HTTPProcessorTest ----------------------------------------------------------------
 HTTPProcessorTest::HTTPProcessorTest(TString tname)
@@ -59,10 +65,6 @@ void HTTPProcessorTest::testDoReadInputWithError()
 	fds.fURISizeLimit = 1024L;
 	testDoReadInputWithCfg( fds, expected );
 
-	String common = String("") << "</head><body bgcolor=\"silver\">\n"
-					<< "<center>\n<h1>Page not found.</h1>\nPress the back button to return to the previous page!<br><br>\n"
-					<< "<FORM><input type=button value=\"Back\" onClick=\"javascript:history.back(1)\"></FORM>\n</center>\n</body></html>";
-
 	expected =    String("") << "HTTP/1.1 414 Request-URL Too Large" << ENDL
 				  << "Connection: close\r\ncontent-type: text/html" << ENDL << ENDL
 				  << "<html><head>\n<title>414 Request-URL Too Large</title>\n"
@@ -70,7 +72,7 @@ void HTTPProcessorTest::testDoReadInputWithError()
 	fds.fRequestSizeLimit = 5120L;
 	fds.fLineSizeLimit = 1024L;
 	fds.fURISizeLimit = 20L;
-	testDoReadInputWithCfg( fds, expected );
+	testDoReadInputWithCfg( fds, expected, false );
 
 	expected =    String("") << "HTTP/1.1 413 Request Entity Too Large" << ENDL
 				  << "Connection: close\r\ncontent-type: text/html" << ENDL << ENDL
@@ -79,7 +81,7 @@ void HTTPProcessorTest::testDoReadInputWithError()
 	fds.fRequestSizeLimit = 5120L;
 	fds.fLineSizeLimit = 30L;
 	fds.fURISizeLimit = 120L;
-	testDoReadInputWithCfg( fds, expected );
+	testDoReadInputWithCfg( fds, expected, false);
 
 	expected =    String("") << "HTTP/1.1 413 Request Entity Too Large" << ENDL
 				  << "Connection: close\r\ncontent-type: text/html" << ENDL << ENDL
@@ -97,7 +99,7 @@ void HTTPProcessorTest::testDoReadInputWithError()
 	fds.fRequestSizeLimit = 200L;
 	fds.fLineSizeLimit = 1024L;
 	fds.fURISizeLimit = 1024L;
-	testDoReadInputWithCfg( fds, expected );
+	testDoReadInputWithCfg( fds, expected, false );
 
 }
 
@@ -109,8 +111,8 @@ void HTTPProcessorTest::testDoReadInputWithCfg( HTTPProcessor &fds, String expec
 	Trace("fLineSizeLimit:" << fds.fLineSizeLimit << ":");
 	Trace("fURISizeLimit:" << fds.fURISizeLimit << ":");
 
-	String uri1;
-	uri1 = 	"GET /hallo123456 HTTP/1.0\r\n"
+	String uri;
+	uri = 	"GET /hallo123456 HTTP/1.0\r\n"
 			"Connection: Keep-Alive\r\n"
 			"User-Agent: Mozilla/4.06 [en] (X11; U; SunOS 5.6 sun4u)\r\n"
 			"Host: sentosa.hsr.loc:1919\r\n"
@@ -119,30 +121,35 @@ void HTTPProcessorTest::testDoReadInputWithCfg( HTTPProcessor &fds, String expec
 			"Accept-Language: en\r\n"
 			"Accept-Charset: iso-8859-1,*,utf-8\r\n"
 			"Cookie: FDState=b64:YnMwOh8tQnIEs1uC2rU3V3fpDOlWqwZeyeSV8xcTT9-m55gkcdmUwEAX5yBjiXEiji1cn63s81FKkm7fz0Sjwnxg-DX7awEQ723P7SKNLpzc4RLj3QBa5t7t6rKawa-X8Y-PYBnJvNByMH3jWcBY3ewDGXM=; FINXSCUSTNO=CH10601\r\n";
-	Anything result1;
+
 	Anything env;
 
-	StringStream is1(&uri1);
+	StringStreamSocket ss(uri);
+	fds.DoReadInput(*(ss.GetStream()), ctx);
+	String reply, fullreply;
+	BuildResult(reply, fullreply, uri);
+	assertEqual( expected, reply );
 
-	Trace("be4: DoReadInput");
-	fds.DoReadInput( is1, ctx);
-	Trace("after: DoReadInput");
-	String reply, fullreply = is1.str();
+	Anything result = ctx.GetRequest();
+	t_assert(expectedRequest == result.LookupPath(env, "env"));
+	t_assert(expectedRequest == result.IsDefined("query"));
+	TraceAny(env, "env");
+	TraceAny(result, "result1");
+}
+
+void HTTPProcessorTest::BuildResult( String &reply, String &fullreply, String result )
+{
+	StartTrace(HTTPProcessorTest.testDoReadInputWithCfg);
+	fullreply << reply << result;
 	long pos = fullreply.Contains("</html>" );
 	if (pos >= 0) {
+
 		Trace("pos" << pos << ":");
 		pos += 7;
 		reply = fullreply.SubString(0L, pos);
 	}
-	Trace("fullreply" << fullreply);
-	Trace("reply" << reply);
-	assertEqual( expected, reply );
-
-	result1 = ctx.GetRequest();
-	t_assert(expectedRequest == result1.LookupPath(env, "env"));
-	t_assert(expectedRequest == result1.IsDefined("query"));
-	TraceAny(env, "env");
-	TraceAny(result1, "result1");
+	Trace("fullreply: " << fullreply);
+	Trace("reply: " 	<< reply);
 }
 
 void HTTPProcessorTest::DoReadInputTest()
@@ -274,25 +281,20 @@ void HTTPProcessorTest::DoReadMinimalInputTest()
 	// ((errornous) Minimal request, the two blanks after GET are critically
 	String uri;
 	uri = 	"GET  HTTP/1.0\r\n\r\n";
-	Anything result;
 
-	StringStream is(&uri);
-	fds.DoReadInput(is, ctx);
-	result = ctx.GetRequest();
+	String expected =    String("") << "HTTP/1.1 400 Bad Request" << ENDL
+						 << "Connection: close\r\ncontent-type: text/html" << ENDL << ENDL
+						 << "<html><head>\n<title>400 Bad Request</title>\n"
+						 << common;
 
-	Anything env;
-	t_assert(result.LookupPath(env, "env"));
-	t_assert(result.IsDefined("query"));
-	TraceAny(env, "env");
-	Anything item;
-	t_assert(env.LookupPath(item, "REQUEST_METHOD"));
-	assertEqual("GET", item.AsCharPtr(""));
+	StringStreamSocket ss(uri);
+	fds.DoReadInput(*(ss.GetStream()), ctx);
+	String reply, fullreply;
+	BuildResult(reply, fullreply, uri);
+	assertEqual( expected, reply );
+	Trace(uri);
+	Trace(expected);
 
-	t_assert(!env.IsDefined("REQUEST_URI"));
-	t_assert(!env.IsDefined("SERVER_PROTOCOL"));
-
-	t_assert(env.LookupPath(item, "header.CONTENT-LENGTH"));
-	assertEqual("0", item.AsCharPtr(""));
 }
 
 void HTTPProcessorTest::FileUploadTest()
