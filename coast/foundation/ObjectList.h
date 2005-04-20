cc3f1c6da52990404ba0b63250cc6ca422653e11
Marcel Huber <marcel.huber@hsr.ch>
2005-04-20 13:53:52 +0000

    based on Modern C++ Design, written by Andrei Alexandrescu, I added a delete method in case the list contains pointers to objects

--- coast/foundation/ObjectList.h
+++ coast/foundation/ObjectList.h
@@ -10,47 +10,55 @@
 #define _ObjectList_H
 
 //---- standard-module include ----------------------------------------------
+#include "config_foundation.h"	// for definition of EXPORTDECL_FOUNDATION
 #include "ITOString.h"
+#include "Dbg.h"
+#if defined(WIN32)
+#include "TypeTraits.h"
+#else
+class NullType {};
+
+namespace Loki
+{
+	template <int v>
+	struct Int2Type {
+		enum { value = v };
+	};
+
+	template <typename T>
+	class TypeTraits
+	{
+	private:
+		template <class U> struct PointerTraits {
+			enum { result = false };
+			typedef NullType PointeeType;
+		};
+
+		template <class U> struct PointerTraits<U *> {
+			enum { result = true };
+			typedef U PointeeType;
+		};
+
+	public:
+		enum { isPointer = PointerTraits<T>::result };
+	};
+};
+#endif
 
 //---- c-module include -----------------------------------------------------
 #include <list>
 
-//---- const ----------------------------------------------------------------
-using namespace std;
-
-class NullType {};
-
-template <typename T>
-class TypeTraits
-{
-private:
-	template <class U> struct PointerTraits {
-		enum { result = false };
-		typedef NullType PointeeType;
-	};
-
-	template <class U> struct PointerTraits<U *> {
-		enum { result = true };
-		typedef U PointeeType;
-	};
-
-public:
-	enum { isPointer = PointerTraits<T>::result };
-	typedef typename PointerTraits<T>::PointeeType PointeeType;
-};
-
-template <int v>
-struct Int2Type {
-	enum { value = v };
-};
+//---- class ObjectList ----------------------------------------------------------------
 
 template<typename Tp>
-class ObjectList : public list<Tp>
+class EXPORTDECL_FOUNDATION ObjectList : public std::list<Tp>
 {
+	// tricky section to determine if given type is a pointer and deletable
+	// ideas from Alexandrescu, loki-lib
 	enum DeleteFuncSelector { Reftype, Pointertype };
 
-	void DoDeleteObject(const typename list<Tp>::value_type &newObjPtr, Int2Type<Reftype> ) {};
-	void DoDeleteObject(const typename list<Tp>::value_type &newObjPtr, Int2Type<Pointertype> ) {
+	void DoDeleteObject(const typename std::list<Tp>::value_type &newObjPtr, Loki::Int2Type<Reftype> ) {};
+	void DoDeleteObject(const typename std::list<Tp>::value_type &newObjPtr, Loki::Int2Type<Pointertype> ) {
 		delete newObjPtr;
 	};
 
@@ -63,23 +71,27 @@ public:
 	{}
 
 	virtual ~ObjectList() {
+		StartTrace1(ObjectList.~ObjectList, (fDestructiveShutdown ? "destructive" : ""));
 		if ( fDestructiveShutdown ) {
 			// no more workers waiting and capable of emptying the list
 			// we have to delete elements if they were pointers, let an appropriate function do this work
-			enum { delAlgo = (TypeTraits<Tp>::isPointer) ? Pointertype : Reftype };
+			enum { delAlgo = (Loki::TypeTraits<Tp>::isPointer) ? Pointertype : Reftype };
 			while ( IntGetSize() > 0 ) {
 				Tp pElement;
 				IntRemoveHead(pElement);
-				DoDeleteObject(pElement, Int2Type<delAlgo>() );
+				DoDeleteObject(pElement, Loki::Int2Type<delAlgo>() );
 			}
 		}
 	}
 
-	virtual bool InsertTail(const typename list<Tp>::value_type &newObjPtr) {
+	virtual bool InsertTail(const typename std::list<Tp>::value_type &newObjPtr) {
+		StartTrace(ObjectList.InsertTail);
 		if ( !IsShuttingDown() ) {
 			push_back(newObjPtr);
+			Trace("pushing object");
 			return true;
 		}
+		Trace("in shutdown");
 		return false;
 	}
 
@@ -88,11 +100,14 @@ public:
 		\param lSecs unused
 		\param lNanosecs unused
 		\return true only when an element could be get, false in case the list was empty or we are in shutdown mode */
-	virtual bool RemoveHead(typename list<Tp>::reference aElement, long lSecs = 0L, long lNanosecs = 0L) {
-		if ( !IsShuttingDown() && !this->empty() ) {
+	virtual bool RemoveHead(typename std::list<Tp>::reference aElement, long lSecs = 0L, long lNanosecs = 0L) {
+		StartTrace(ObjectList.RemoveHead);
+		if ( !IsShuttingDown() && !IntIsEmpty() ) {
 			IntRemoveHead(aElement);
+			Trace("element removed");
 			return true;
 		}
+		Trace("in shutdown or empty list");
 		return false;
 	}
 
@@ -103,6 +118,7 @@ public:
 	/*! Method to be called to signal that we want to destroy the list. If the elements should still be consumed by the workers reading from the list false should be passed as argument. If a 'destructive' shutdown is wanted true should be passed.
 		\param bDestructive set to true if the list should not be emptied by the workers and not accessed further */
 	inline virtual void SignalShutdown(bool bDestructive = false) {
+		StartTrace1(ObjectList.SignalShutdown, (bDestructive ? "destructive" : ""));
 		fDestructiveShutdown = bDestructive;
 		fShutdown = true;
 	}
@@ -111,14 +127,22 @@ public:
 		return fShutdown;
 	}
 
+	inline virtual void ResetAfterShutdown() {
+		fShutdown = false;
+		fDestructiveShutdown = false;
+	}
+
 protected:
-	virtual void IntRemoveHead(typename list<Tp>::reference aElement) {
+	virtual void IntRemoveHead(typename std::list<Tp>::reference aElement) {
 		aElement = this->front();
 		this->pop_front();
 	}
 
 	inline virtual size_t IntGetSize() const {
 		return this->size();
+	}
+	inline virtual bool IntIsEmpty() const {
+		return this->empty();
 	}
 
 	bool			fShutdown;
