c7dff794019e06232f1bd2d87facadde561a3b66
Marcel Huber <marcel.huber@hsr.ch>
2005-04-20 13:57:31 +0000

    corrected template parameters to keep newer compilers happy adjusted some baseclass methods

--- coast/mtfoundation/ObjectList_r.h
+++ coast/mtfoundation/ObjectList_r.h
@@ -9,19 +9,20 @@
 #ifndef _ObjectList_r_H
 #define _ObjectList_r_H
 
+//---- baseclass include -----------------------------------------------------
+#include "config_mtfoundation.h"
+#include "ObjectList.h"
+
 //---- standard-module include ----------------------------------------------
 #include "SysLog.h"
 #include "Threads.h"
-
-//---- baseclass include -----------------------------------------------------
-#include "ObjectList.h"
 
 //---- c-module include -----------------------------------------------------
 
 //---- class ObjectList_r ----------------------------------------------------------------
 
 template<typename Tp>
-class ObjectList_r : public ObjectList<Tp>
+class EXPORTDECL_MTFOUNDATION ObjectList_r : public ObjectList<Tp>
 {
 public:
 	ObjectList_r(const char *name, Allocator *a = Storage::Global())
@@ -30,23 +31,27 @@ public:
 	{}
 
 	virtual ~ObjectList_r() {
+		StartTrace1(ObjectList_r.~ObjectList_r, (ObjectList<Tp>::fDestructiveShutdown ? "destructive" : ""));
 		SimpleMutexEntry me(fMutex);
-		fShutdown = true;
-		if ( fDestructiveShutdown == false ) {
+		ObjectList<Tp>::fShutdown = true;
+		if ( ObjectList<Tp>::fDestructiveShutdown == false ) {
 			// let the workers empty the list and destruct its elements
-			while ( !this->empty() ) {
+			while ( !ObjectList<Tp>::IntIsEmpty() ) {
 				fCondEmpty.Wait(fMutex);
 			}
 		}
 		// if the list was in destructive shutdown, let the baseclass remove and destruct the elements
 	}
 
-	virtual bool InsertTail(const typename list<Tp>::value_type &newObjPtr) {
+	virtual bool InsertTail(const typename std::list<Tp>::value_type &newObjPtr) {
+		StartTrace(ObjectList_r.InsertTail);
 		SimpleMutexEntry me(fMutex);
-		if ( !IsShuttingDown() && ObjectList<Tp>::InsertTail(newObjPtr) ) {
+		if ( !ObjectList<Tp>::IsShuttingDown() && ObjectList<Tp>::InsertTail(newObjPtr) ) {
 			fCondFull.BroadCast();
+			Trace("success");
 			return true;
 		}
+		Trace("failure");
 		return false;
 	}
 
@@ -55,40 +60,47 @@ public:
 		\param lSecs time in seconds after which the wait will be aborted, specify 0 for endless wait
 		\param lNanosecs time in nanoseconds (10e-9) after which the wait will be aborted, specify 0 for endless wait
 		\return true only when an element could be get, false in case the list was empty, we are in shutdown mode or a timeout occured */
-	virtual bool RemoveHead(typename list<Tp>::reference aElement, long lSecs = 0L, long lNanosecs = 0L) {
+	virtual bool RemoveHead(typename std::list<Tp>::reference aElement, long lSecs = 0L, long lNanosecs = 0L) {
+		StartTrace(ObjectList_r.RemoveHead);
 		SimpleMutexEntry me(fMutex);
-		while ( !IsShuttingDown() && this->empty() ) {
+		// wait on new element
+		while ( !ObjectList<Tp>::IsShuttingDown() && ObjectList<Tp>::IntIsEmpty() ) {
 			if ( fCondFull.TimedWait(fMutex, lSecs, lNanosecs) == TIMEOUTCODE ) {
-				// prematurely exit in case of a timeout
+				Trace("premature exit because of a timeout and empty list");
 				return false;
 			}
 		}
-		if ( !IsShuttingDown() && ObjectList<Tp>::RemoveHead(aElement) ) {
+		if ( !ObjectList<Tp>::IsShuttingDown() && ObjectList<Tp>::RemoveHead(aElement) ) {
 			fCondEmpty.Signal();
+			Trace("got an element");
 			return true;
 		}
+		Trace("in shutdown");
 		return false;
 	}
 
 	/*! returns the current number of elements in the list
 		/return number of elements */
 	virtual size_t GetSize() {
+		StartTrace(ObjectList_r.GetSize);
 		size_t tmpSz = 0;
 		{
 			SimpleMutexEntry me(fMutex);
-			tmpSz = IntGetSize();
+			tmpSz = ObjectList<Tp>::IntGetSize();
 		}
+		Trace("current size:" << (long)tmpSz);
 		return tmpSz;
 	}
 
 	/*! Method to be called to signal that we want to destroy the list. If the elements should still be consumed by the workers reading from the list false should be passed as argument. If a 'destructive' shutdown is wanted true should be passed.
 		\param bDestructive set to true if the list should not be emptied by the workers, otherways the call blocks until the list empty */
 	virtual void SignalShutdown(bool bDestructive = false) {
+		StartTrace1(ObjectList_r.SignalShutdown, (bDestructive ? "destructive" : ""));
 		{
 			SimpleMutexEntry me(fMutex);
 			if ( bDestructive == false ) {
-				// wait for workerThreads to drain the list
-				while (this->size() > 0) {
+				Trace("waiting on workerThreads to drain the list");
+				while ( !ObjectList<Tp>::IntIsEmpty() ) {
 					fCondEmpty.Wait(fMutex);
 				}
 			}
