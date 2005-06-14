928fe6b263156944af606453d5d0948c25e08bc8
Markus Pfister <pfm@bluewin.ch>
2005-06-14 10:06:48 +0000

    removed typo in test switches

--- coast/modules/SSL/Test/config/hank_cert/docopy.sh
+++ coast/modules/SSL/Test/config/hank_cert/docopy.sh
@@ -1,8 +1,8 @@
 #!/bin/ksh
 # Use this script to install new generated certificates.
 cp /tmp/mkca/ca-hierarchy/ucerts/* .
-clienthost=dylan.hank.com
-serverhost=dylan.hank.com
+clienthost=dylan.hsr.ch
+serverhost=dylan.hsr.ch
 cp clientcrt_${clienthost}.pem clientcrt.pem
 cp clientkey_${clienthost}.pem clientkey.pem
 cp servercrt_${serverhost}.pem servercrt.pem
--- coast/modules/StdDataAccess/Test/config/hank_cert/docopy.sh
+++ coast/modules/StdDataAccess/Test/config/hank_cert/docopy.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-clienthost="dylan.hank.com"
+clienthost="dylan.hsr.ch"
 cp /tmp/mkca/ca-hierarchy/ucerts/clientcrt_${clienthost}.pem clientcrt.pem
 cp /tmp/mkca/ca-hierarchy/ucerts/clientkey_${clienthost}.pem clientkey.pem
 cp /tmp/mkca/ca-hierarchy/ucerts/rootCA1crt.pem .
