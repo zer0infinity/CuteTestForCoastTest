1d0e4affaa63336679ff4e6a9f0a000f002e5d27
Marcel Huber <marcel.huber@hsr.ch>
2006-06-09 21:43:06 +0000

    conditional result putting mapper  - compares two renderable values with given operator

---
+++ coast/modules/DataAccess/CompareValuesMapper.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2006, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+//--- interface include --------------------------------------------------------
+#include "CompareValuesMapper.h"
+
+//--- project modules used -----------------------------------------------------
+
+//--- standard modules used ----------------------------------------------------
+#include "Renderer.h"
+#include "SysLog.h"
+#include "Dbg.h"
+
+//--- c-modules used -----------------------------------------------------------
+
+//---- CompareValuesMapper ------------------------------------------------------------------
+RegisterResultMapper(CompareValuesMapper);
+
+CompareValuesMapper::CompareValuesMapper(const char *name)
+	: ResultMapper(name)
+{
+	StartTrace(CompareValuesMapper.Ctor);
+}
+
+IFAObject *CompareValuesMapper::Clone() const
+{
+	return new CompareValuesMapper(fName);
+}
+
+bool CompareValuesMapper::DoPutAny(const char *key, Anything value, Context &ctx, ROAnything script)
+{
+	StartTrace1(CompareValuesMapper.DoPutAny, NotNull(key));
+	ROAnything roaRightValue, roaLeftValue, roaOperator;
+	if ( Lookup("RightValue", roaRightValue) && Lookup("LeftValue", roaLeftValue) && Lookup("Operator", roaOperator) ) {
+		String strRightValue, strOperator, strLeftValue;
+		TraceAny(roaLeftValue, "spec how to obtain value to compare");
+		{
+			Context::PushPopEntry aEntry(ctx, "PutterValue", value);
+			Renderer::RenderOnString(strLeftValue, ctx, roaLeftValue);
+			Renderer::RenderOnString(strRightValue, ctx, roaRightValue);
+		}
+		Renderer::RenderOnString(strOperator, ctx, roaOperator);
+
+		Trace("now testing for [" << strLeftValue << " " << strOperator << " " << strRightValue << "]");
+		bool bPutResult = false;
+		if ( strOperator.IsEqual(">") ) {
+			bPutResult = (strLeftValue.AsLong(0L) > strRightValue.AsLong(0L));
+		} else if ( strOperator.IsEqual("<") ) {
+			bPutResult = (strLeftValue.AsLong(0L) < strRightValue.AsLong(0L));
+		} else if ( strOperator.IsEqual(">=") ) {
+			bPutResult = (strLeftValue.AsLong(0L) >= strRightValue.AsLong(0L));
+		} else if ( strOperator.IsEqual("<=") ) {
+			bPutResult = (strLeftValue.AsLong(0L) <= strRightValue.AsLong(0L));
+		} else if ( strOperator.IsEqual("==") ) {
+			bPutResult = (strLeftValue.AsLong(0L) == strRightValue.AsLong(0L));
+		} else if ( strOperator.IsEqual("!=") ) {
+			bPutResult = (strLeftValue.AsLong(0L) != strRightValue.AsLong(0L));
+		}
+		if ( bPutResult ) {
+			Trace("comparison yields true, putting result");
+			return ResultMapper::DoPutAny(key, value, ctx, script);
+		}
+		// result skipped
+		return true;
+	}
+	SYSWARNING("not all required slots for mapper given");
+	return false;
+}
---
+++ coast/modules/DataAccess/CompareValuesMapper.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2006, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+#ifndef _CompareValuesMapper_H
+#define _CompareValuesMapper_H
+
+//---- ResultMapper include -------------------------------------------------
+#include "config_dataaccess.h"
+#include "Mapper.h"
+
+//---- CompareValuesMapper ----------------------------------------------------------
+//! <B>Mapper to conditionally put a result based on a configurable comparison operation</B>
+/*!
+<B>Configuration for configured Mapper in OutputMapperMeta.any:</B><PRE>
+{
+	/LeftValue		Rendererspec	mandatory, how to obtain the left comparison value from Context including 'pushed' value to put
+	/RightValue		Rendererspec	mandatory, how to obtain the right comparison value from Context including 'pushed' value to put
+	/Operator		Rendererspec	mandatory, ['==','!=','<','>','<=','>='], operator to compare left with right value
+}</PRE>
+
+Example:
+<br><i>OutputMapperMeta.any contains</i>
+<pre>
+/SomeConfiguredMapperForADataAccess {
+	...
+	/QueryResult {
+		/CompareValuesMapper *
+	}
+	...
+}
+...
+/CompareValuesMapper {
+	/PutPolicy	Append				# <== base mapper feature
+	/LeftValue	5
+	/RightValue { /Lookup Value }
+	/Operator ">="
+}</pre>
+
+Results after DataAccess without using this Mapper:
+<pre>
+/QueryResult {
+	{
+		/Value "5"
+	}
+	{
+		/Value "3"
+	}
+	{
+		/Value "12"
+	}
+	{
+		/Value "7"
+	}
+}</pre>
+
+Results after DataAccess using this Mapper:
+<pre>
+/QueryResult {
+	{
+		/Value "5"
+	}
+	{
+		/Value "12"
+	}
+	{
+		/Value "7"
+	}
+}</pre>
+*/
+class EXPORTDECL_DATAACCESS CompareValuesMapper : public ResultMapper
+{
+public:
+	//--- constructors
+	CompareValuesMapper(const char *name);
+	//--- support for prototype
+	IFAObject *Clone() const;
+
+protected:
+	//! Major hook for subclasses that want to do something with their config passed as script. The default is to interpret the script and put a value for every script item used. Recursion will be stopped by DoFinalPutAny which places its value under slot key below given DoGetDestinationSlot()
+	/*! \param key the key usually defines the associated kind of output-value
+		\param value the value to be mapped
+		\param ctx the context of the invocation
+		\param script current mapper configuration as ROAnything
+		\return returns true if the mapping was successful otherwise false */
+	virtual bool DoPutAny(const char *key, Anything value, Context &ctx, ROAnything script);
+
+private:
+	CompareValuesMapper();
+	CompareValuesMapper(const CompareValuesMapper &);
+	CompareValuesMapper &operator=(const CompareValuesMapper &);
+};
+
+#endif
