982de3b6b0fd556e9b9fc14c9d88d064be68488f
Markus Pfister <pfm@bluewin.ch>
2007-02-14 14:49:25 +0000

    Generate a unique id. Added testcases

---
+++ coast/modules/Security/UniqueIdGen.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2007, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+//--- interface include --------------------------------------------------------
+#include "UniqueIdGen.h"
+
+//--- project modules used -----------------------------------------------------
+
+//--- standard modules used ----------------------------------------------------
+#include "Dbg.h"
+#include "MD5.h"
+#include "SystemAPI.h"
+#include "System.h"
+#include "DiffTimer.h"
+#include "StringStream.h"
+
+//--- c-library modules used ---------------------------------------------------
+#if !defined(WIN32)
+#include <sys/time.h>
+#endif
+
+//--- c-modules used -----------------------------------------------------------
+
+//---- UniqueIdGen ----------------------------------------------------------------
+UniqueIdGen::UniqueIdGen()
+{
+	StartTrace(UniqueIdGen.Ctor);
+}
+
+UniqueIdGen::~UniqueIdGen()
+{
+	StartTrace(UniqueIdGen.Dtor);
+}
+
+String UniqueIdGen::GetUniqueId(const String &additionalToken)
+{
+	StartTrace(UniqueIdGen.GetUniqueId);
+	timeval tv;
+	tv.tv_sec	= 0;
+	tv.tv_usec	= 0;
+	long seconds		= 0L;
+	long nanoSeconds	= 0L;
+	if ( 0 == gettimeofday(&tv, 0) ) {
+		nanoSeconds += tv.tv_usec * 1000; // make it nanoseconds
+		seconds		+= tv.tv_sec;
+	} else {
+		seconds += time(0);	// fall back towards using time
+	}
+	unsigned long ms = 	(seconds * 1000000L) + nanoSeconds;
+	srand48(GetHRTIME());
+	unsigned long random = lrand48();
+	unsigned long hrt = GetHRTIME();
+	long hostid = ::gethostid();
+	long pid = System::getpid();
+	char buf[1024];
+	sprintf(buf, "%u_%u_%u_%u_%u", ms, hrt, hostid, pid, random);
+	String uniqueId(buf);
+	uniqueId << "_" << additionalToken;
+	Trace("uniqueId: [" << uniqueId << "]");
+	MD5Signer md5("UniqueIdGen");
+	String ret(HexHash(md5, uniqueId));
+	Trace("Resulting UniqueId: [" << ret << "]");
+	return ret;
+}
+
+String UniqueIdGen::HexHash(MD5Signer &md5, const char *s)
+{
+	StartTrace(Utils.HexHash);
+	String hash;
+	md5.DoHash(s, hash);
+	OStringStream os;
+	for (long i = 0 ; i < hash.Length(); i++) {
+		os << hex <<  setw(2) << setfill('0') << (int)(unsigned char)hash[i];
+	}
+	return os.str();
+}
---
+++ coast/modules/Security/UniqueIdGen.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2007, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+#ifndef _UniqueIdGen_H
+#define _UniqueIdGen_H
+
+//---- baseclass include -------------------------------------------------
+
+//---- forward declaration -----------------------------------------------
+class MD5Signer;
+
+//---- other includes      -----------------------------------------------
+#include "ITOString.h"
+
+//---- UniqueIdGen ----------------------------------------------------------
+//! <B>Generate a Unique ID</B>
+/*!
+Returns a String containing a unique id.
+*/
+class UniqueIdGen
+{
+public:
+	//--- constructors
+	UniqueIdGen();
+	~UniqueIdGen();
+	//! Generate a UniqueId. You can pass an additional token, e.g. the calling
+	//! thread's id.
+	static String GetUniqueId(const String &additionalToken = "NoAdditionaTokenWasSet");
+
+protected:
+	static String HexHash(MD5Signer &md5, const char *s);
+
+private:
+	//! inhibit compiler supplied bitwise default copy ctor
+	UniqueIdGen(const UniqueIdGen &);
+	//! inhibit compiler supplied bitwise default assignment operator
+	UniqueIdGen &operator=(const UniqueIdGen &);
+};
+
+#endif
