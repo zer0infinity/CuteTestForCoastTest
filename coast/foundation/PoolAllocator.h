51f08a3782c06a85b464e63d47f0eac13961a414
Marcel Huber <marcel.huber@hsr.ch>
2007-03-06 16:59:27 +0000

    corrected comment about NumOfPoolBucketSizes param and resulting maximal block size used

--- coast/foundation/PoolAllocator.h
+++ coast/foundation/PoolAllocator.h
@@ -66,7 +66,7 @@ public:
 	/*! create and initialize a pool allocator
 		\param poolid use poolid to distinguish more than one pool
 		\param poolSize size of pre-allocated pool in kBytes, default 1MByte
-		\param maxKindOfBucket number of different allocation units within PoolAllocator, starts at 16 bytes and doubles the size for maxKindOfBucket times. So maxKindOfBucket=10 will give a max block size of 16384 bytes. */
+		\param maxKindOfBucket number of different allocation units within PoolAllocator, starts at 16 bytes and doubles the size for maxKindOfBucket times. So maxKindOfBucket=10 will give a max block size of 8192 bytes. */
 	PoolAllocator(long poolid, u_long poolSize = 1024, u_long maxKindOfBucket = 10);
 	//! destroy a pool only if its empty, i.e. all allocated bytes are freed
 	virtual ~PoolAllocator();
