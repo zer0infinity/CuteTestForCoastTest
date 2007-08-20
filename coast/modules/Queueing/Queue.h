3a20876d251a30c6bd27754f274ba41dcc38ca8b
Marcel Huber <marcel.huber@hsr.ch>
2007-08-20 17:02:24 +0000

    templateified Queue to allow elements of any type to be stored, therefor the container type is also a template parameter  - element and container types can be different from Anything  - AnyQueueType is a predefined type for old Anything based Queue instances

--- coast/modules/Queueing/Queue.h
+++ coast/modules/Queueing/Queue.h
@@ -13,10 +13,13 @@
 #include "config_Queueing.h"
 #include "IFAObject.h"
 #include "Threads.h"
+#include "Dbg.h"
+#include "SysLog.h"
+#include "DateTime.h"
+
+#include <algorithm>
 #include <limits.h>
-
 //---- forward declaration -----------------------------------------------
-
 //---- Queue ----------------------------------------------------------
 //! <B>Simple, thread-safe, Anything based queue</B>
 /*!
@@ -46,13 +49,64 @@ Statistics can be made by evaluating the returned Anything from GetStatistics().
 </pre>
 <b>* marks slots not yet available</b>
 */
-class EXPORTDECL_QUEUEING Queue : public IFAObject
+template <
+class TElementType,
+	  class TListStorageType
+	  >
+class QueueBase : public IFAObject
 {
 	friend class QueueTest;
 public:
+	typedef TElementType ElementType;
+	typedef ElementType &ElementTypeRef;
+	typedef TListStorageType ListStorageType;
+	typedef ListStorageType &ListStorageTypeRef;
+	typedef QueueBase<ElementType, ListStorageType> ThisType;
+
 	//--- constructors
-	Queue(const char *name, long lQueueSize = LONG_MAX, Allocator *pAlloc = Storage::Global());
-	~Queue();
+	QueueBase(const char *name, long lQueueSize = LONG_MAX, Allocator *pAlloc = Storage::Global())
+		: fName(name, -1, pAlloc)
+		, fAllocator(pAlloc)
+		, fSemaFullSlots(0L)
+		, fSemaEmptySlots(lQueueSize)
+		, fPutCount(0L)
+		, fGetCount(0L)
+		, fMaxLoad(0L)
+		, fBlockingPutCount(0L)
+		, fBlockingGetCount(0L)
+		, fAlive(0xf007f007)
+		, feBlocked(eBothSides)
+		, fQueueLock("QueueMutex", fAllocator)
+		, fBlockedLock("BlockedLock", fAllocator)
+		, fBlockingPutLock("BlockingPutLock", fAllocator)
+		, fBlockingGetLock("BlockingGetLock", fAllocator)
+		, fAnyQueue()
+		, fAnyStatistics(fAllocator) {
+		StartTrace1(Queue.Queue, "queue size:" << lQueueSize);
+		fAnyStatistics["QueueSize"] = lQueueSize;
+		Anything anyTime;
+		DateTime::GetTimeOfDay(anyTime);
+		fAnyStatistics["CreateTime"]["sec"] = anyTime["sec"].AsLong(0L);
+		fAnyStatistics["CreateTime"]["usec"] = anyTime["usec"].AsLong(0L);
+		UnBlock();
+	}
+
+	~QueueBase() {
+		StartTrace(Queue.~Queue);
+		// block any pending interface accesses
+		Block();
+
+		LockUnlockEntry me(fQueueLock);
+		// mark that we are no longer alive
+		fAlive = 0x00dead00;
+
+		long lSize(IntGetSize());
+		if ( lSize > 0L ) {
+			SYSWARNING("Destruction of non-empty queue [" << fName << "]! still " << lSize << " Elements in Queue!");
+			ListStorageType anyElements;
+			IntEmptyQueue(anyElements);
+		}
+	}
 
 	/*! Status code of queue Put and Get operations. As we support blocking of either read and/or write side of the queue, we must return appropriate codes telling the caller what happened. A simple boolean value is far not enough to find out why an operation failed. */
 	enum StatusCode {
@@ -65,21 +119,110 @@ public:
 		eTryAcquireFailed = eAcquireFailed << 1,
 		eDead = eTryAcquireFailed << 1,
 	};
-	/*! further explanation of the purpose of the method
-		this may contain <B>HTML-Tags</B>
-		...
-		\param aParam explanation of aParam (important : paramname - explanation )
-		\return explanation of return value
-		\pre explanation of precondition for the method call
-		\post explanation of postcondition for the method call
-	*/
-	StatusCode Put(Anything &anyElement, bool bTryLock = false);
-	StatusCode Get(Anything &anyElement, bool bTryLock = false);
+	StatusCode Put(ElementTypeRef anyElement, bool bTryLock = false) {
+		StartTrace(Queue.Put);
+		StatusCode eRet(eBlocked);
+		if ( !IsBlocked(ePutSide) ) {
+			if ( bTryLock ) {
+				eRet = /* eTryAcquireFailed |*/ eFull;
+				if ( fSemaEmptySlots.TryAcquire() ) {
+					eRet = DoPut(anyElement);
+				}
+			} else {
+				LockedValueIncrementDecrementEntry ce(fBlockingPutLock, fBlockingPutCond, fBlockingPutCount);
+				// need double checking here because of possible race condition during destruction on fBlockingPutLock
+				if ( IsAlive() && fSemaEmptySlots.Acquire() ) {
+					eRet = eDead;
+					if ( IsAlive() ) {
+						eRet = DoPut(anyElement);
+					} else {
+						// must release semaphore again because we did not put an element
+						fSemaEmptySlots.Release();
+					}
+				}
+			}
+		}
+		return eRet;
+	}
 
-	void EmptyQueue(Anything &anyElements);
+	StatusCode Get(ElementTypeRef anyElement, bool bTryLock = false) {
+		StartTrace(Queue.Get);
+		StatusCode eRet(eBlocked);
+		if ( !IsBlocked(eGetSide) ) {
+			if ( bTryLock ) {
+				eRet = /*eTryAcquireFailed |*/ eEmpty;
+				if ( fSemaFullSlots.TryAcquire() ) {
+					eRet = DoGet(anyElement);
+				}
+			} else {
+				LockedValueIncrementDecrementEntry ce(fBlockingGetLock, fBlockingGetCond, fBlockingGetCount);
+				// need double checking here because of possible race condition during destruction on fBlockingGetLock
+				eRet = eAcquireFailed;
+				if ( IsAlive() && fSemaFullSlots.Acquire() ) {
+					eRet = eDead;
+					if ( IsAlive() ) {
+						eRet = DoGet(anyElement);
+					} else {
+						// must release semaphore again because we did not get an element
+						fSemaFullSlots.Release();
+					}
+				}
+			}
+		}
+		return eRet;
+	}
 
-	long GetSize();
-	void GetStatistics(Anything &anyStatistics);
+	void EmptyQueue(ListStorageTypeRef anyElements) {
+		StartTrace(Queue.EmptyQueue);
+		if ( !IsBlocked() ) {
+			LockUnlockEntry me(fQueueLock);
+			this->IntEmptyQueue(anyElements);
+		}
+	}
+	long GetSize() {
+		StartTrace(Queue.GetSize);
+		long lSize = -1;
+		if ( !IsBlocked() ) {
+			LockUnlockEntry me(fQueueLock);
+			lSize = IntGetSize();
+		}
+		return lSize;
+	}
+
+	void GetStatistics(Anything &anyStatistics) {
+		StartTrace(Queue.GetStatistics);
+		if ( IsAlive() ) {
+			long lCurSec(0L), lCurUSec(0L);
+			{
+				LockUnlockEntry me(fQueueLock);
+				// must pass a DeepClone because of reference counting
+				anyStatistics = fAnyStatistics.DeepClone(anyStatistics.GetAllocator());
+				// this Anything uses Storage::Current as default allocator so we must take care
+				// when we use a Thread specific PoolAllocator because the Pool access is not locked
+				Anything anyTime;
+				DateTime::GetTimeOfDay(anyTime);
+				lCurSec = anyTime["sec"].AsLong(0L);
+				lCurUSec = anyTime["usec"].AsLong(0L);
+			}
+			anyStatistics["MaxLoad"] = fMaxLoad;
+			anyStatistics["PutCount"] = (long)fPutCount;
+			anyStatistics["GetCount"] = (long)fGetCount;
+			anyStatistics["CurrentSize"] = IntGetSize();
+			anyStatistics["StatisticTime"]["sec"] = lCurSec;
+			anyStatistics["StatisticTime"]["usec"] = lCurUSec;
+
+			long lSecDiff(lCurSec - anyStatistics["CreateTime"]["sec"].AsLong());
+			long lUSecDiff(lCurUSec - anyStatistics["CreateTime"]["usec"].AsLong());
+			if ( lUSecDiff < 0L ) {
+				--lSecDiff;
+				lUSecDiff += 1000000L;
+			}
+			anyStatistics["LifeTime"]["sec"] = lSecDiff;
+			anyStatistics["LifeTime"]["usec"] = lUSecDiff;
+
+			TraceAny(anyStatistics, "statistics");
+		}
+	}
 
 	inline bool IsAlive() {
 		return fAlive == 0xf007f007;
@@ -92,21 +235,138 @@ public:
 		eBothSides = ePutSide | eGetSide,
 	};
 
-	bool IsBlocked(BlockingSide aSide = eBothSides);
-	void Block(BlockingSide aSide = eBothSides);
-	void UnBlock(BlockingSide aSide = eBothSides);
+	bool IsBlocked(BlockingSide aSide = eBothSides) {
+		StartTrace(Queue.IsBlocked);
+		if ( IsAlive() ) {
+			// need only a read lock here
+			LockUnlockEntry rwe(fBlockedLock, RWLock::eReading);
+			return ( ( feBlocked & aSide ) == aSide );
+		}
+		return true;
+	}
+
+	void Block(BlockingSide aSide = eBothSides) {
+		StartTrace1(Queue.Block, "side:" << aSide);
+		if ( IsAlive() ) {
+			{
+				// need a write lock here
+				LockUnlockEntry rwe(fBlockedLock, RWLock::eWriting);
+				feBlocked = ( BlockingSide )( feBlocked | ( aSide & eBothSides ) );
+			}
+			// release potentially waiting putters/getters
+			LockUnlockEntry me(fQueueLock);
+			if ( aSide & ePutSide ) {
+				IntReleaseBlockedPutters();
+			}
+			if ( aSide & eGetSide ) {
+				IntReleaseBlockedGetters();
+			}
+		}
+	}
+
+	void UnBlock(BlockingSide aSide = eBothSides) {
+		StartTrace1(Queue.UnBlock, "side:" << aSide);
+		if ( IsAlive() ) {
+			// need a write lock here
+			LockUnlockEntry rwe(fBlockedLock, RWLock::eWriting);
+			feBlocked = ( BlockingSide )( feBlocked & ( ~aSide & eBothSides ) );
+		}
+	}
 
 	virtual IFAObject *Clone() const {
 		return NULL;
 	}
 
-private:
-	StatusCode DoPut(Anything &anyElement);
-	StatusCode DoGet(Anything &anyElement);
-	long IntGetSize();
-	void IntEmptyQueue(Anything &anyElements);
-	void IntReleaseBlockedPutters();
-	void IntReleaseBlockedGetters();
+protected:
+	StatusCode DoPut(ElementTypeRef anyElement) {
+		StartTrace(Queue.DoPut);
+		StatusCode eRet(eBlocked);
+		if ( !IsBlocked(ePutSide) ) {
+			LockUnlockEntry me(fQueueLock);
+			fAnyQueue.push_back(anyElement);
+			++fPutCount;
+			fMaxLoad = std::max( fMaxLoad, (long)fAnyQueue.size() );
+			fSemaFullSlots.Release();
+			eRet = eSuccess;
+		}
+		return eRet;
+	}
+
+	StatusCode DoGet(ElementTypeRef anyElement) {
+		StartTrace(Queue.DoGet);
+		StatusCode eRet(eBlocked);
+		if ( !IsBlocked(eGetSide) ) {
+			LockUnlockEntry me(fQueueLock);
+			if ( fAnyQueue.size() ) {
+				anyElement = fAnyQueue.front();	//TODO - future: change here so that it's not restricted only for Anything's - use any_cast
+				fAnyQueue.pop_front();
+				++fGetCount;
+				fSemaEmptySlots.Release();
+				eRet = eSuccess;
+			} else {
+				// we can only get here if something curious happens with semaphore handling
+				// because we were able to acquire a full-slot just before, something must have happened at this point
+				// which should not be possible by its design
+				// -> someone is destructing the queue without blocking first?
+				SYSERROR("accessed empty Queue!?");
+				eRet = ( StatusCode )( eEmpty | eError );
+			}
+		}
+		return eRet;
+	}
+
+	long IntGetSize() {
+		StartTrace(Queue.IntGetSize);
+		long lRet(fAnyQueue.size());
+		Trace("Anything  size:" << lRet);
+		Trace("(Put-Get) size:" << (long)(fPutCount - fGetCount));
+		return lRet;
+	}
+
+	void IntEmptyQueue(ListStorageTypeRef anyElements) {
+		StartTrace(Queue.IntEmptyQueue);
+		Trace("current queue size:" << IntGetSize());
+		while ( IntGetSize() ) {
+			fSemaFullSlots.TryAcquire();
+			moveElement(fAnyQueue, anyElements);
+			fSemaEmptySlots.Release();
+		}
+
+		Trace("elements removed:" << (long)fAnyQueue.size());
+	}
+
+	void moveElement(ListStorageTypeRef aFrom, ListStorageTypeRef aTo) {
+		aTo.push_back(aFrom.front());
+		aFrom.pop_front();
+	}
+
+	void IntReleaseBlockedPutters() {
+		StartTrace(Queue.IntReleaseBlockedPutters);
+		LockUnlockEntry sme(fBlockingPutLock);
+		if ( fBlockingPutCount ) {
+			SYSWARNING("waking up " << fBlockingPutCount << " blocked Put() callers");
+		}
+		while ( fBlockingPutCount ) {
+			Trace("releasing one of " << fBlockingPutCount << " blocked Putters");
+			fSemaEmptySlots.Release();
+			// allow signalled thread to decrement the fBlockingPutCount after failed return from Put
+			fBlockingPutCond.Wait(fBlockingPutLock);
+		}
+	}
+
+	void IntReleaseBlockedGetters() {
+		StartTrace(Queue.IntReleaseBlockedGetters);
+		LockUnlockEntry sme(fBlockingGetLock);
+		if ( fBlockingGetCount ) {
+			SYSINFO("waking up " << fBlockingGetCount << " blocked Get() callers");
+		}
+		while ( fBlockingGetCount ) {
+			Trace("releasing one of " << fBlockingGetCount << " blocked Getters");
+			fSemaFullSlots.Release();
+			// allow signalled thread to decrement the fBlockingGetCount after failed return from Get
+			fBlockingGetCond.Wait(fBlockingGetLock);
+		}
+	}
 
 	String		fName;
 	Allocator	*fAllocator;
@@ -119,7 +379,52 @@ private:
 	RWLock		fBlockedLock;
 	SimpleMutex fBlockingPutLock, fBlockingGetLock;
 	SimpleMutex::ConditionType fBlockingPutCond, fBlockingGetCond;
-	Anything 	fAnyQueue, fAnyStatistics;
+	ListStorageType fAnyQueue;
+	Anything	fAnyStatistics;
 };
+
+template <
+class TElementType,
+	  class TListStorageType
+	  >
+class Queue : public QueueBase<TElementType, TListStorageType>
+{
+	friend class QueueTest;
+public:
+	typedef TElementType ElementType;
+	typedef ElementType &ElementTypeRef;
+	typedef TListStorageType ListStorageType;
+	typedef ListStorageType &ListStorageTypeRef;
+	typedef QueueBase<ElementType, ListStorageType> BaseType;
+	typedef Queue<ElementType, ListStorageType> ThisType;
+
+	Queue(const char *name, long lQueueSize = LONG_MAX, Allocator *pAlloc = Storage::Global())
+		: BaseType(name, lQueueSize, pAlloc) {
+		StartTrace1(Queue.Queue, "generic");
+	}
+};
+
+template <
+class TElementType
+>
+class Queue<TElementType, Anything> : public QueueBase<TElementType, Anything>
+{
+	friend class QueueTest;
+public:
+	typedef TElementType ElementType;
+	typedef ElementType &ElementTypeRef;
+	typedef Anything ListStorageType;
+	typedef ListStorageType &ListStorageTypeRef;
+	typedef QueueBase<ElementType, ListStorageType> BaseType;
+	typedef Queue<ElementType, ListStorageType> ThisType;
+
+	Queue(const char *name, long lQueueSize = LONG_MAX, Allocator *pAlloc = Storage::Global())
+		: BaseType(name, lQueueSize, pAlloc) {
+		StartTrace1(Queue.Queue, "Anything");
+		this->fAnyQueue.SetAllocator(pAlloc);
+	}
+};
+
+typedef Queue<Anything, Anything> AnyQueueType;
 
 #endif
