19b7506528c9277c401c19e2f9f50fb85fc811f7
Marcel Huber <marcel.huber@hsr.ch>
2007-08-20 17:51:50 +0000

    templateified QueueWorkingModule  - use AnyQueueWorkingModule for a predefined Anything/Anything combination module

--- coast/modules/Queueing/QueueWorkingModule.h
+++ coast/modules/Queueing/QueueWorkingModule.h
@@ -11,10 +11,11 @@
 
 //---- WDModule include -------------------------------------------------
 #include "config_Queueing.h"
-#include "WDModule.h"
 #include "Context.h"
+#include "Server.h"
 #include "Queue.h"
 #include "AppLog.h"
+#include "StringStream.h"
 
 //---- QueueWorkingModule ----------------------------------------------------------
 //! <B>Queue based module for message passing systems</B>
@@ -28,81 +29,297 @@
 	...
 }</PRE>
 */
-//template
-//<
-//	class ElementType,
-//	class ListStorageType
-//>
-class EXPORTDECL_QUEUEING QueueWorkingModule : public WDModule
+template
+<
+class ElementType,
+	  class ListStorageType
+	  >
+class QueueWorkingModule : public WDModule
 {
 	friend class QueueWorkingModuleTest;
 	friend class ValueOutputtingModuleTest;
 	friend class CalculationsModuleTest;
 public:
-	typedef Anything ElementType;
-	typedef Anything ListStorageType;
 	typedef ElementType &ElementTypeRef;
 	typedef Queue<ElementType, ListStorageType> QueueType;
-	typedef QueueType::ListStorageTypeRef ListStorageTypeRef;
+	typedef typename QueueType::ListStorageTypeRef ListStorageTypeRef;
 	typedef QueueType &AnyQueueTypeRef;
 	typedef QueueType *AnyQueueTypePtr;
+	typedef typename QueueType::StatusCode StatusCode;
+	typedef typename QueueType::BlockingSide BlockingSide;
 
 	//--- constructors
-	QueueWorkingModule(const char *name);
-	~QueueWorkingModule();
+	QueueWorkingModule(const char *name)
+		: WDModule(name)
+		, fpQueue(NULL)
+		, fpQAllocator(NULL)
+		, fpContext(NULL)
+		, fContextLock("QueueWorkingModuleContextLock")
+		, fFailedPutbackMessages(Storage::Global())
+		, fAlive(0UL) {
+		StartTrace(QueueWorkingModule.QueueWorkingModule);
+	}
 
 	//! implementers should initialize module using config
-	virtual bool Init(const ROAnything config);
-	//! implementers should terminate module expecting destruction
-	virtual bool Finis();
-	//! initializes module after termination for reinitialization; default uses Init; check if this applies
-	virtual bool ResetInit(const ROAnything config);
-	//! terminates module for reinitialization; default uses Finis; check if this applies
-	virtual bool ResetFinis(const ROAnything config);
+	virtual bool Init(const ROAnything config) {
+		StartTrace(QueueWorkingModule.Init);
+		SubTraceAny(FullConfig, config, "Config: ");
+		ROAnything roaConfig;
+		if ( config.LookupPath(roaConfig, fName) ) {
+			fConfig = roaConfig.DeepClone();
+			TraceAny(fConfig, "Module config");
+			fErrorLogName = GetNamedConfig("Logging")["ErrorChannel"].AsCharPtr("ErrorLog");
+			fWarningLogName = GetNamedConfig("Logging")["WarningChannel"].AsCharPtr("WarningLog");
+			fInfoLogName = GetNamedConfig("Logging")["InfoChannel"].AsCharPtr("AccessLog");
+			const char *pServerName = GetNamedConfig("Logging")["Servername"].AsCharPtr("Server");
+			Server *pServer = Server::FindServer(pServerName);
+			Anything dummy;
+			fpContext = new Context(fConfig, dummy, pServer, 0, 0);
+			IntInitQueue(GetConfig());
+			fAlive = 0xf007f007;
+			return true;
+		}
+		return false;
+	}
 
-	inline bool IsAlive() {
+	//! implementers should terminate module expecting destruction
+	virtual bool Finis() {
+		StartTrace(QueueWorkingModule.Finis);
+
+		SetDead();
+
+		Anything anyStat;
+		if ( GetQueueStatistics(anyStat) ) {
+			StringStream aStream;
+			aStream << "Statistics for [" << GetName() << "]\n";
+			anyStat.PrintOn(aStream, true) << "\n" << std::flush;
+			SysLog::WriteToStderr(aStream.str());
+		}
+
+		// delete Queue, also wakes up blocked threads, threads MUST be in termination sequence!!
+		IntCleanupQueue();
+
+		{
+			LockUnlockEntry me(fContextLock);
+			delete fpContext;
+			fpContext = NULL;
+		}
+
+		return true;
+	}
+
+	//! initializes module after termination for reinitialization; default uses Init; check if this applies
+	virtual bool ResetInit(const ROAnything config) {
+		StartTrace(QueueWorkingModule.ResetInit);
+		// calls Init
+		return WDModule::ResetInit(config);
+	}
+
+	//! terminates module for reinitialization; default uses Finis; check if this applies
+	virtual bool ResetFinis(const ROAnything config) {
+		StartTrace(QueueWorkingModule.ResetFinis);
+		// calls Finis
+		return WDModule::ResetFinis(config);
+	}
+
+	bool IsAlive() {
 		return fAlive == 0xf007f007;
 	}
 
-	bool IsBlocked(QueueType::BlockingSide aSide = QueueType::eBothSides);
+	bool IsBlocked(BlockingSide aSide = QueueType::eBothSides) {
+		StartTrace(QueueWorkingModule.IsBlocked);
+		bool bRet = false;
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			bRet = fpQueue->IsBlocked(aSide);
+		}
+		return bRet;
+	}
 
-	void Block(QueueType::BlockingSide aSide);
+	void Block(BlockingSide aSide) {
+		StartTrace(QueueWorkingModule.Block);
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			fpQueue->Block(aSide);
+		}
+	}
 
-	void UnBlock(QueueType::BlockingSide aSide);
+	void UnBlock(BlockingSide aSide) {
+		StartTrace(QueueWorkingModule.UnBlock);
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			fpQueue->UnBlock(aSide);
+		}
+	}
 
 	/*! main accessor functions to work with the queue */
-	QueueType::StatusCode PutElement(ElementTypeRef anyELement, bool bTryLock = false);
-	QueueType::StatusCode GetElement(ElementTypeRef anyValues, bool bTryLock = false);
-	void PutBackElement(ElementTypeRef anyValues);
+	StatusCode PutElement(ElementTypeRef anyELement, bool bTryLock = false) {
+		StartTrace(QueueWorkingModule.PutElement);
+		StatusCode eRet = QueueType::eDead;
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			eRet = fpQueue->Put(anyELement, bTryLock);
+			if ( eRet != QueueType::eSuccess ) {
+				SYSWARNING("Queue->Put failed, QueueType::StatusCode:" << eRet << " !");
+			}
+		}
+		return eRet;
+	}
+
+	StatusCode GetElement(ElementTypeRef anyValues, bool bTryLock = false) {
+		StartTrace(QueueWorkingModule.GetElement);
+		StatusCode eRet = QueueType::eDead;
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			Trace("Queue still alive");
+			// try to get a failed message first
+			if ( fFailedPutbackMessages.GetSize() > 0 ) {
+				Trace("getting failed message 1 of " << fFailedPutbackMessages.GetSize() );
+				anyValues = fFailedPutbackMessages[0L];
+				fFailedPutbackMessages.Remove(0L);
+				eRet = QueueType::eSuccess;
+			} else {
+				// Default is blocking get to save cpu time
+				eRet = fpQueue->Get(anyValues, bTryLock);
+				if ( ( eRet != QueueType::eSuccess ) && ( eRet != QueueType::eEmpty ) ) {
+					SYSWARNING("Queue->Get failed, QueueType::StatusCode:" << eRet << " !");
+				}
+			}
+		}
+		return eRet;
+	}
+
+	void PutBackElement(ElementTypeRef anyValues) {
+		StartTrace(QueueWorkingModule.PutBackElement);
+		// put message back to the queue (Appends!) if possible
+		// take care not to lock ourselves up here, thus we MUST use a trylock here!
+		StatusCode eRet = PutElement(anyValues, true);
+		if ( eRet != QueueType::eSuccess && ( eRet & QueueType::eFull ) ) {
+			// no more room in Queue, need to store this message internally for later put back
+			fFailedPutbackMessages.Append(anyValues);
+		}
+	}
 
 	/* exclusively consume all Elements from queue, threads which are blocked on the queue to get an element will be woken up because of the released semaphore. But instead of getting an Element it will get nothing back and should be able to handle this correctly.
 		\param anyELements Anything to hold the elements removed from the queue
 		\return number of elements removed from the queue */
-	long FlushQueue(ListStorageTypeRef anyElements);
+	long FlushQueue(ListStorageTypeRef anyElements) {
+		StartTrace(QueueWorkingModule.FlushQueue);
+		long lElements = 0L;
+		if ( fpQueue && fpQueue->IsAlive() && IsAlive() ) {
+			fpQueue->EmptyQueue(anyElements);
+			lElements = anyElements.GetSize();
+			LogInfo(String(GetName()) << ": flushed " << lElements << " elements");
+		}
+		return lElements;
+	}
 
-	bool GetQueueStatistics(Anything &anyStat);
+	bool GetQueueStatistics(Anything &anyStat) {
+		StartTrace(QueueWorkingModule.GetQueueStatistics);
+		if ( fpQueue ) {
+			fpQueue->GetStatistics(anyStat);
+			return true;
+		}
+		return false;
+	}
 
-	long GetCurrentSize();
+	long GetCurrentSize() {
+		StartTrace(QueueWorkingModule.GetCurrentSize);
+		long lSize = 0L;
+		if ( fpQueue ) {
+			lSize = fpQueue->GetSize();
+		}
+		return lSize;
+	}
 
-	void LogError(String strMessage);
-	void LogWarning(String strMessage);
-	void LogInfo(String strMessage);
+	void LogError(String strMessage) {
+		StartTrace(QueueWorkingModule.LogError);
+		SYSERROR(strMessage);
+		Log(strMessage, fErrorLogName, AppLogModule::eERROR);
+	}
+
+	void LogWarning(String strMessage) {
+		StartTrace(QueueWorkingModule.LogWarning);
+		SYSWARNING(strMessage);
+		Log(strMessage, fWarningLogName, AppLogModule::eWARNING);
+	}
+
+	void LogInfo(String strMessage) {
+		StartTrace(QueueWorkingModule.LogInfo);
+		Log(strMessage, fInfoLogName, AppLogModule::eINFO);
+	}
 
 protected:
-	ROAnything GetNamedConfig(const char *name);
-	ROAnything GetConfig();
+	ROAnything GetNamedConfig(const char *name) {
+		StartTrace(QueueWorkingModule.GetNamedConfig);
+		return ((ROAnything)fConfig)[name];
+	}
+
+	ROAnything GetConfig() {
+		StartTrace(QueueWorkingModule.GetConfig);
+		return ((ROAnything)fConfig);
+	}
+
 	Context *GetContext() {
 		return fpContext;
 	};
-	void IntCleanupQueue();
+
+	void IntCleanupQueue() {
+		StartTrace(QueueWorkingModule.IntCleanupQueue);
+		// we could do something here to persist the content of the queue and the putback message buffer
+		delete fpQueue;
+		fpQueue = NULL;
+		if ( fpQAllocator ) {
+			MT_Storage::UnrefAllocator(fpQAllocator);
+			fpQAllocator = NULL;
+		}
+		fFailedPutbackMessages = Anything();
+	}
+
 	void SetDead() {
 		fAlive = 0x00dead00;
 	};
-	void Log(String strMessage, const char *channel, AppLogModule::eLogLevel iLevel);
-	void Log(Anything &anyStatus, const char *channel, AppLogModule::eLogLevel iLevel);
+
+	void Log(String strMessage, const char *channel, AppLogModule::eLogLevel iLevel) {
+		StartTrace(QueueWorkingModule.Log);
+		Trace(strMessage);
+		if ( IsAlive() && fpContext ) {
+			LockUnlockEntry me(fContextLock);
+			if ( IsAlive() && fpContext ) {
+				fpContext->GetTmpStore()["LogMessage"] = strMessage;
+				AppLogModule::Log(*fpContext, channel, iLevel);
+			}
+		}
+	}
+
+	void Log(Anything &anyStatus, const char *channel, AppLogModule::eLogLevel iLevel) {
+		StartTrace(QueueWorkingModule.Log);
+		TraceAny(anyStatus, "content to log");
+		if ( IsAlive() && fpContext ) {
+			LockUnlockEntry me(fContextLock);
+			if ( IsAlive() && fpContext ) {
+				fpContext->GetTmpStore()["QueueWorkingStatus"] = anyStatus;
+				AppLogModule::Log(*fpContext, channel, iLevel);
+			}
+		}
+	}
 
 private:
-	void IntInitQueue(const ROAnything roaConfig);
+	void IntInitQueue(const ROAnything roaConfig) {
+		StartTrace(QueueWorkingModule.IntInitQueue);
+		long lQueueSize = roaConfig["QueueSize"].AsLong(100L);
+		Allocator *pAlloc = Storage::Global();
+
+		if ( roaConfig["UsePoolStorage"].AsLong(0) == 1 ) {
+			// create unique allocator id based on a pointer value
+			long lAllocatorId = (((long)this) & 0x00007FFF);
+			pAlloc = MT_Storage::MakePoolAllocator(roaConfig["PoolStorageSize"].AsLong(10240), roaConfig["NumOfPoolBucketSizes"].AsLong(10), lAllocatorId);
+			if ( pAlloc == NULL ) {
+				SYSERROR("was not able to create PoolAllocator with Id:" << lAllocatorId << " for [" << GetName() << "], check config!");
+			} else {
+				// store allocator pointer for later deletion
+				MT_Storage::RefAllocator(pAlloc);
+				fpQAllocator = pAlloc;
+			}
+		}
+		fpQueue = new QueueType(GetName(), lQueueSize, pAlloc);
+		fFailedPutbackMessages = Anything();
+	}
 
 	Anything	fConfig;
 	AnyQueueTypePtr	fpQueue;
@@ -114,7 +331,6 @@ private:
 	u_long		fAlive;
 };
 
-typedef QueueWorkingModule AnyQueueWorkingModule;
-//typedef QueueWorkingModule<Anything, Anything> AnyQueueWorkingModule;
+typedef QueueWorkingModule<Anything, Anything> AnyQueueWorkingModule;
 
 #endif
