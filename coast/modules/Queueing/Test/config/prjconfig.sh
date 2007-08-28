241d3a62c1c4d103cce5da982ec94dd2b34c6233
Marcel Huber <marcel.huber@hsr.ch>
2007-08-28 22:23:20 +0000

    set TRACE_STORAGE to 1 to enable storage tests

--- coast/modules/Queueing/Test/config/prjconfig.sh
+++ coast/modules/Queueing/Test/config/prjconfig.sh
@@ -26,6 +26,20 @@
 # overwrite this one ONLY if the WD_PATH variable points to the wrong place
 #WD_PATH=config
 
+# The flag WD_USE_MMAP_STREAMS controls the usage of memory mapped files. Default is to use mmap streams
+#  because for most operations and conditions this seems to be fast.
+# When setting this variable to 0, fstreams will be used instead
+# note: Memory mapped files will always increase the file size by an internally managed blocksize,
+#  on SunOS_5.8, this blocksize seems to be 8192 bytes. If you intend to use a tail -f on these files
+#  you will probably not get what you expect. tail can not handle the reserved - and still unused - space.
+#export WD_USE_MMAP_STREAMS=0
+
+# The flag TRACE_STORAGE defines the logging level of memory statistics
+#  0: No pool statistic tracing, except when excess memory was used
+#  1: Trace overall statistics
+#  2: Trace detailed statistics
+export TRACE_STORAGE=1
+
 # use this function to do preparations prior to creating the distribution package
 # e.g. increment a build number
 #function preDoallFunc {}
