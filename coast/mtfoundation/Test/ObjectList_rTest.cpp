119d13a695dcc6eec5b69ae91273cc6b03be5b8f
Marcel Huber <marcel.huber@hsr.ch>
2007-09-10 21:44:06 +0000

    extended tests to test different type of list/allocator combinations

--- coast/mtfoundation/Test/ObjectList_rTest.cpp
+++ coast/mtfoundation/Test/ObjectList_rTest.cpp
@@ -19,6 +19,7 @@
 #include "Dbg.h"
 
 //--- c-modules used -----------------------------------------------------------
+#include <deque>
 
 //---- ObjectList_rTest ----------------------------------------------------------------
 ObjectList_rTest::ObjectList_rTest(TString tstrName) : TestCaseType(tstrName)
@@ -31,44 +32,141 @@ ObjectList_rTest::~ObjectList_rTest()
 	StartTrace(ObjectList_rTest.Dtor);
 }
 
+#define OBJECTLISTMACRO(listtype, allocatortype)\
+{\
+	TraceMemDelta("before allocation");\
+	const long lCount=200;\
+	MemChecker mc("ObjectList_rTest.CtorTest", Storage::Global());\
+	{\
+		Trace(_QUOTE_( testing listtype##allocatortype ));\
+		ObjectList_r<String*, listtype, allocatortype> aStringList(_QUOTE_( listtype##allocatortype ));\
+		String *pString;\
+		pString=new String("Guguseli");\
+		aStringList.InsertTail(pString);\
+		assertCompare(1L, equal_to, (long)aStringList.GetSize());\
+		TraceMemDelta("before deallocation");\
+		if ( t_assert(aStringList.RemoveHead(pString)) )\
+		{\
+			delete pString;\
+		}\
+		assertCompare(0L, equal_to, (long)aStringList.GetSize());\
+		TraceMemDelta("after deallocation");\
+		for (long lIdx=0; lIdx < lCount; ++lIdx)\
+		{\
+			pString = new String("Iteration:");\
+			pString->Append(lIdx);\
+			aStringList.InsertTail(pString);\
+		}\
+		assertCompare(lCount, equal_to, (long)aStringList.GetSize());\
+		TraceMemDelta("after reallocation of many strings");\
+		for (long lIdx2=0; lIdx2 < (lCount/2); ++lIdx2)\
+		{\
+			if ( aStringList.RemoveHead(pString) )\
+			{\
+				Trace("current value [" << *pString << "]");\
+				assertCharPtrEqual(String("Iteration:").Append(lIdx2), *pString);\
+				delete pString;\
+			}\
+		}\
+		TraceMemDelta("after removal of many strings");\
+		assertCompare(0LL, less, mc.CheckDelta());\
+		pString=NULL;\
+		Trace("Remove another element which should succeed"); \
+		if ( t_assertm(aStringList.RemoveHead(pString, 0L, 10000000L), "expected RemoveHead to still succeed") )\
+		{\
+			delete pString;\
+		}\
+		Trace("destructive shutdown of list, expect remaining elements getting deleted"); \
+		aStringList.SignalShutdown(true);\
+	}\
+	assertCompare(0LL, equal_to, mc.CheckDelta());\
+}
+
 void ObjectList_rTest::CtorTest()
 {
 	StartTrace(ObjectList_rTest.CtorTest);
 	StartTraceMem(ObjectList_rTest.CtorTest);
 	{
-		TraceMemDelta("before allocation");
-		String *pString;
-		const long lCount = 10;
-		pString = new String("Guguseli");
-		ObjectList_r<String *> aStringList("StringPointerList_r");
-		aStringList.InsertTail(pString);
-		assertEqual(1, aStringList.GetSize());
-		TraceMemDelta("before deallocation");
-		if ( t_assert(aStringList.RemoveHead(pString)) ) {
-			delete pString;
-		}
-		assertEqual(0, aStringList.GetSize());
-		TraceMemDelta("after deallocation");
-		for (long lIdx = 0; lIdx < lCount; lIdx++) {
-			pString = new String("Iteration:");
-			pString->Append(lIdx);
-			aStringList.InsertTail(pString);
-		}
-		assertEqual(lCount, aStringList.GetSize());
-		TraceMemDelta("after reallocation of many strings");
-		for (long lIdx2 = 0; lIdx2 < lCount; lIdx2++) {
-			if ( aStringList.RemoveHead(pString) ) {
-				Trace("current value [" << *pString << "]");
-				assertCharPtrEqual(String("Iteration:").Append(lIdx2), *pString);
-				delete pString;
+		// Allocate several tester objects.  Delete two.
+		Trace("with deque. . .");
+		MemChecker mc("deque-test", Storage::Global());
+		{
+#if defined(__GNUG__)  && ( __GNUC__ >= 4 )
+			std::deque<String *, STLStorage::fast_pool_allocator_global<String *> > l;
+#else
+			std::deque<String *, std::allocator<String *> > l;
+#endif
+			String *pS(NULL);
+			for (long i = 0; i < 150; ++i) {
+				pS = new String("gaga");
+				pS->Append(i);
+				l.push_back(pS);
 			}
+			delete l.back();
+			Trace("deleted back");
+			l.pop_back();
+			delete l.front();
+			Trace("deleted front");
+			l.pop_front();
+			assertCompare(148L, equal_to, (long)l.size());
+			for (int j = 0; j < (150 - 2); ++j) {
+				String *pS = l.front();
+				if ( pS ) {
+					Trace(*pS);
+				}
+				delete pS;
+				l.pop_front();
+			}
+			Trace("clearing list");
+			l.clear();
 		}
-		TraceMemDelta("after removal of many strings");
-		pString = NULL;
-		// wait on Remove for 10ms but it should fail
-		t_assertm(!aStringList.RemoveHead(pString, 0L, 10000000L), "expected empty list!");
-		t_assert(pString == NULL);
+		assertCompare(0LL, equal_to, mc.CheckDelta());
 	}
+	{
+		// Allocate several tester objects.  Delete two.
+		Trace("with list. . .");
+		MemChecker mc("list-test", Storage::Global());
+		{
+#if defined(__GNUG__)  && ( __GNUC__ >= 4 )
+			std::list<String *, STLStorage::fast_pool_allocator_global<String *> > l;
+#else
+			std::list<String *, std::allocator<String *> > l;
+#endif
+			String *pS(NULL);
+			for (long i = 0; i < 150; ++i) {
+				pS = new String("gaga");
+				pS->Append(i);
+				l.push_back(pS);
+			}
+			delete l.back();
+			Trace("deleted back");
+			l.pop_back();
+			delete l.front();
+			Trace("deleted front");
+			l.pop_front();
+			assertCompare(148L, equal_to, (long)l.size());
+			for (int j = 0; j < (150 - 2); ++j) {
+				String *pS = l.front();
+				if ( pS ) {
+					Trace(*pS);
+				}
+				delete pS;
+				l.pop_front();
+			}
+			Trace("clearing list");
+			l.clear();
+		}
+		assertCompare(0LL, equal_to, mc.CheckDelta());
+	}
+#if defined(__GNUG__)  && ( __GNUC__ >= 4 )
+	OBJECTLISTMACRO(std::list, STLStorage::pool_allocator_global);
+	OBJECTLISTMACRO(std::list, STLStorage::fast_pool_allocator_global);
+	Trace("continuing ObjectList_rTests");
+	OBJECTLISTMACRO(std::deque, STLStorage::pool_allocator_global);
+	OBJECTLISTMACRO(std::deque, STLStorage::fast_pool_allocator_global);
+#else
+	OBJECTLISTMACRO(std::list, std::allocator);
+#endif
 	TraceMemDelta("before terminating");
 }
 
