c8b4fe05fe6bf3b74258f89109451ce1d58415e9
Marcel Huber <marcel.huber@hsr.ch>
2007-10-19 13:16:22 +0000

    added another template parameter to specify locking policy  - this eliminates the need for users of this class to handle locking externally

--- coast/foundation/set_wrapper.hpp
+++ coast/foundation/set_wrapper.hpp
@@ -12,11 +12,12 @@
 //---- baseclass include -------------------------------------------------
 #include <loki/Visitor.h>
 
-#include "Anything.h"
 #include "STLStorage.h"
 #include <set>
 
 //---- forward declaration -----------------------------------------------
+class Anything;
+class LockUnlockEntry;
 
 //---- set_wrapper ----------------------------------------------------------
 //! <B>single line description of the class</B>
@@ -24,8 +25,90 @@
 further explanation of the purpose of the class
 this may contain <B>HTML-Tags</B>
 */
+class DummyCondition;
+
+class DummyMutex
+{
+	friend class DummyCondition;
+public:
+	typedef DummyCondition ConditionType;
+	/*! create mutex with names to ease debugging of locking problems
+		\param name a name to identify the mutex when tracing locking problems
+		\param a allocator used to allocate the storage for the name */
+	DummyMutex(const char *, Allocator *) {}
+	~DummyMutex() {}
+
+	/*! tries to acquire the mutex for this thread, blocks the caller if already locked (by another thread)
+		acquires the mutex for the calling thread; this is NOT recursive, if you call lock twice for the same thread it deadlocks */
+	void Lock() {}
+
+	/*! releases the mutex */
+	void Unlock() {}
+
+	/*! tries to acquire the mutex if possible; bails out without locking if already locked
+		\return false when already locked */
+	bool TryLock() {
+		return true;
+	}
+
+//	/*! returns native reprentation of mutex hidden by macro MUTEXPTR */
+//	MUTEXPTR GetInternal() const {  return GETMUTEXPTR(fMutex); }
+
+private:
+	//!standard constructor prohibited
+	DummyMutex();
+	//!standard copy constructor prohibited
+	DummyMutex(const DummyMutex &);
+	//!standard assignement operator prohibited
+	void operator=(const DummyMutex &);
+};
+
+class DummyCondition
+{
+public:
+	typedef DummyMutex MutexType;
+	//!creates a system dependent condition variable
+	DummyCondition() {}
+	//!destroys the system dependent condition variable
+	~DummyCondition() {}
+
+	/*! waits for the condition to be signaled atomicly releasing the mutex while waiting and reaquiring it when returning from the wait
+		\param m the mutex which is used with the condition; it must be locked, since it will be unlocked
+		\return system dependent return value; zero if call was ok */
+	int Wait(MutexType &) {
+		return 0;
+	}
+
+	/*! waits for the condition to be signaled at most secs seconds and nanosecs nanoseconds if available on the plattform. After this time we block on the SimpleMutex until we can lock it before leaving the function
+		\param m the mutex which is used with the condition; it must be locked, since it will be unlocked
+		\param secs timeout to wait in seconds
+		\param nanosecs timeout to wait in nanoseconds (10e-9)
+		\return system dependent return value; zero if call was ok */
+	int TimedWait(MutexType &, long, long) {
+		return 0;
+	}
+
+	//!access the system dependent handle to the condition variable
+//	CONDPTR GetInternal() { return GETCONDPTR(fSimpleCondition); }
+	//!signal the condition; do it while protected through the associated mutex, then unlock the mutex; zero or one waiting thread will be woken up
+	int Signal() {
+		return 0;
+	}
+	//!broadcasts the condition; do it while protected through the associated mutex, then unlock the mutex; all waiting thread will eventually be woken up
+	int BroadCast() {
+		return 0;
+	}
+
+private:
+	//!get internal 'Id' mainly for Tracing
+	long GetId() {
+		return -1;
+	}
+};
+
 template <
 typename T,
+		 class MutexPolicy = DummyMutex,
 		 template < typename > class AllocType = DefaultAllocatorGlobalType
 		 >
 class set_wrapper : public Loki::BaseVisitable< Anything >
@@ -45,32 +128,28 @@ public:
 
 	LOKI_DEFINE_VISITABLE()
 
-	set_wrapper()
+	set_wrapper( const char *pName = "set_wrapper_lock" )
 		: fpList()
+		, fLock( pName, Storage::Global() )
 	{}
 
 	bool AddItem(const ItemType &aItem) {
-		InsertResultType aRetCode( GetCreateListPtr()->insert(aItem) );
-		StatTrace(set_wrapper.AddItem, "Item [" << aItem.AsString() << "] was " << (aRetCode.second ? "" : "not " ) << "successful", Storage::Current());
 		// success means it was a new entry, otherwise there is already an item present
-		return aRetCode.second;
+		LockUnlockEntry aGuard( fLock );
+		return IntAddItem( aItem );
 	}
 
 	bool DeleteItem(const ItemType &aItem) {
 		StartTrace(set_wrapper.DeleteItem);
-		bool bRet(false);
-		if ( HasList() ) {
-			size_type aRetCode( GetCreateListPtr()->erase(aItem) );
-			// number of elements == 1 means success
-			bRet = ( aRetCode == 1 );
-		}
-		return bRet;
+		LockUnlockEntry aGuard( fLock );
+		return IntDeleteItem( aItem );
 	}
 
 	bool ReplaceItem(const ItemType &aItem) {
 		StartTrace(set_wrapper.ReplaceItem);
-		DeleteItem(aItem);
-		return AddItem(aItem);
+		LockUnlockEntry aGuard( fLock );
+		IntDeleteItem(aItem);
+		return IntAddItem(aItem);
 	}
 
 	/*! this is the exact match version of find
@@ -80,8 +159,9 @@ public:
 		StartTrace(set_wrapper.FindItem);
 		bool bFound(false);
 		if ( HasList() ) {
-			rItemPos = GetListPtr()->find( aItem );
-			bFound = ( rItemPos != GetListPtr()->end() );
+			LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+			rItemPos = IntGetConstListPtr()->find( aItem );
+			bFound = ( rItemPos != IntGetConstListPtr()->end() );
 		}
 		return bFound;
 	}
@@ -93,15 +173,8 @@ public:
 		StartTrace1(set_wrapper.FindItemRange, "find items for range [" << aItemMin.AsString() << "," << aItemMax.AsString() << "]");
 		bool bFound(false);
 		if ( HasList() ) {
-			ConstIteratorType aEnd( GetListPtr()->end() );
-			rItemsBegin = GetListPtr()->lower_bound( aItemMin );
-			rItemsEnd = GetListPtr()->upper_bound( aItemMax );
-			Trace("[" << ( ( rItemsBegin != aEnd ) ? (*rItemsBegin).AsString() : "<end>" ) << "," << ( ( rItemsEnd != aEnd ) ? (*rItemsEnd).AsString() : "<end>" ) << ")");
-			if ( ( rItemsBegin != aEnd || rItemsEnd != aEnd ) ) {
-				// check if values are within range
-				// -> begin value must at least be lower or equal the given aItemMax
-				bFound = ( *rItemsBegin < aItemMax || !( aItemMax < *rItemsBegin ) );
-			}
+			LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+			bFound = IntFindItemRange( aItemMin, aItemMax, rItemsBegin, rItemsEnd );
 		}
 		return bFound;
 	}
@@ -113,8 +186,9 @@ public:
 		StartTrace(set_wrapper.FullRange);
 		bool bFound(false);
 		if ( HasList() ) {
-			rItemsBegin = GetCreateListPtr()->begin();
-			rItemsEnd = GetCreateListPtr()->end();
+			LockUnlockEntry aGuard( fLock );
+			rItemsBegin = IntGetCreateListPtr()->begin();
+			rItemsEnd = IntGetCreateListPtr()->end();
 			bFound = ( rItemsBegin != rItemsEnd );
 		}
 		return bFound;
@@ -124,7 +198,8 @@ public:
 		StartTrace1(set_wrapper.DeleteItemRange, "[" << ( ( rItemsBegin != fpList->end() ) ? (*rItemsBegin).AsString() : "<end>" ) << "," << ( ( rItemsEnd != fpList->end() ) ? (*rItemsEnd).AsString() : "<end>" ) << ")");
 		bool bRet(false);
 		if ( HasList() ) {
-			GetCreateListPtr()->erase(rItemsBegin, rItemsEnd);
+			LockUnlockEntry aGuard( fLock );
+			IntGetCreateListPtr()->erase(rItemsBegin, rItemsEnd);
 			bRet = true;
 		}
 		return bRet;
@@ -134,51 +209,130 @@ public:
 		StartTrace1(set_wrapper.DeleteItemRange, "deleting items for range [" << aItemMin.AsString() << "," << aItemMax.AsString() << "]");
 		bool bRet(false);
 		if ( HasList() ) {
+			LockUnlockEntry aGuard( fLock );
 			IteratorType rItemsBegin, rItemsEnd;
-			if ( FindItemRange(aItemMin, aItemMax, rItemsBegin, rItemsEnd) ) {
-				GetCreateListPtr()->erase(rItemsBegin, rItemsEnd);
+			if ( IntFindItemRange(aItemMin, aItemMax, rItemsBegin, rItemsEnd) ) {
+				IntGetCreateListPtr()->erase(rItemsBegin, rItemsEnd);
 				bRet = true;
 			}
 		}
 		return bRet;
 	}
 
-	size_type Size() const {
-		if ( this->HasList() ) {
-			return this->GetListPtr()->size();
+	void Clear() {
+		LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+		if ( this->IntHasList() ) {
+			this->IntGetListPtr()->clear();
 		}
-		return size_type(0);
+	}
+
+	size_type Size() const {
+		LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+		size_type sz(0);
+		if ( this->IntHasList() ) {
+			sz = this->IntGetConstListPtr()->size();
+		}
+		return sz;
 	}
 
 	bool HasList() const {
-		return ( fpList.get() != NULL );
+		LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+		return IntHasList();
 	}
 
 	bool IsEnd(const IteratorType &aIter) const {
-		return ( HasList() ? ( aIter == fpList->end() ) : true );
+		LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+		return ( IntHasList() ? ( aIter == fpList->end() ) : true );
 	}
 
-	const Type *GetListPtr() const {
-		return fpList.get();
+	const Type *GetConstListPtr() const {
+		LockUnlockEntry aGuard( *const_cast<MutexPolicy *>(&fLock) );
+		return IntGetConstListPtr();
 	}
 
-	template < template < class > class A >
-	set_wrapper &operator=(const set_wrapper<T, A>& other) {
+	set_wrapper &operator=(const set_wrapper &other) {
+		return Assign( other );
+	}
+	template < class M, template < class > class A >
+	set_wrapper &operator=(const set_wrapper<T, M, A>& other) {
+		return Assign( other );
+	}
+
+	template < class M, template < class > class A >
+	set_wrapper &Assign(const set_wrapper<T, M, A>& other) {
 		if ( other.HasList() ) {
-			CopyItems(GetCreateListPtr(), other.GetListPtr());
+			LockUnlockEntry aGuard( fLock );
+			CopyItems(IntGetCreateListPtr(), other.GetConstListPtr());
 		}
 		return (*this);
 	}
 
 	Type *GetCreateListPtr() {
-		if ( !HasList() ) {
+		LockUnlockEntry aGuard( fLock );
+		return IntGetCreateListPtr();
+	}
+
+protected:
+	bool IntHasList() const {
+		return ( fpList.get() != NULL );
+	}
+
+	Type *IntGetListPtr() {
+		return fpList.get();
+	}
+
+	const Type *IntGetConstListPtr() const {
+		return fpList.get();
+	}
+
+	Type *IntGetCreateListPtr() {
+		if ( !IntHasList() ) {
 			fpList = std::auto_ptr< Type >( new Type() );
 		}
 		return fpList.get();
 	}
 
+	/*! No compare version of find, do not compare VAT/STT/Value attributes of Item
+		\param
+		\return */
+	bool IntFindItemRange(const ItemType &aItemMin, const ItemType &aItemMax, ConstIteratorType &rItemsBegin, ConstIteratorType &rItemsEnd) const {
+		StartTrace1(set_wrapper.IntFindItemRange, "find items for range [" << aItemMin.AsString() << "," << aItemMax.AsString() << "]");
+		bool bFound(false);
+		if ( IntHasList() ) {
+			ConstIteratorType aEnd( IntGetConstListPtr()->end() );
+			rItemsBegin = IntGetConstListPtr()->lower_bound( aItemMin );
+			rItemsEnd = IntGetConstListPtr()->upper_bound( aItemMax );
+			Trace("[" << ( ( rItemsBegin != aEnd ) ? (*rItemsBegin).AsString() : "<end>" ) << "," << ( ( rItemsEnd != aEnd ) ? (*rItemsEnd).AsString() : "<end>" ) << ")");
+			if ( ( rItemsBegin != aEnd || rItemsEnd != aEnd ) ) {
+				// check if values are within range
+				// -> begin value must at least be lower or equal the given aItemMax
+				bFound = ( *rItemsBegin < aItemMax || !( aItemMax < *rItemsBegin ) );
+			}
+		}
+		return bFound;
+	}
+
+	bool IntAddItem(const ItemType &aItem) {
+		InsertResultType aRetCode( IntGetCreateListPtr()->insert(aItem) );
+		StatTrace(set_wrapper.IntAddItem, "Item [" << aItem.AsString() << "] was " << (aRetCode.second ? "" : "not " ) << "successful", Storage::Current());
+		// success means it was a new entry, otherwise there is already an item present
+		return aRetCode.second;
+	}
+
+	bool IntDeleteItem(const ItemType &aItem) {
+		StartTrace(set_wrapper.IntDeleteItem);
+		bool bRet(false);
+		if ( IntHasList() ) {
+			size_type aRetCode( IntGetCreateListPtr()->erase(aItem) );
+			// number of elements == 1 means success
+			bRet = ( aRetCode == 1 );
+		}
+		return bRet;
+	}
+
 private:
 	std::auto_ptr< Type > fpList;
+	MutexPolicy fLock;
 };
 
 template < class AL, class AR >
