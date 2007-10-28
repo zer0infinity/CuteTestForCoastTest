d5eb726154a028b89b761069b2b0f7277084e237
Zeno Albisser <zeno@albisser.org>
2007-10-28 00:44:32 +0000

    moved Actions Test sources to separate directory

--- coast/modules/Actions/Test/src/LoopTestAction.h
+++ coast/modules/Actions/Test/src/LoopTestAction.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * Copyright (c) 2007, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
  * All rights reserved.
  *
  * This library/application is free software; you can redistribute and/or modify it under the terms of
