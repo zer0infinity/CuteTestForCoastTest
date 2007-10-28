0af97240f1612d089457e02a26fed0c213398e25
Zeno Albisser <zeno@albisser.org>
2007-10-28 01:35:33 +0000

    replaced SysLog.h with SystemLog.h

--- coast/modules/Actions/TransitionDispatchAction.cpp
+++ coast/modules/Actions/TransitionDispatchAction.cpp
@@ -11,7 +11,7 @@
 
 //--- standard modules used ----------------------------------------------------
 #include "Renderer.h"
-#include "SysLog.h"
+#include "SystemLog.h"
 #include "Dbg.h"
 
 //--- c-modules used -----------------------------------------------------------
