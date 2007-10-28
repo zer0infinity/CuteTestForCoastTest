f1437439d092bd667e618cd323c0208cc0a244b9
Zeno Albisser <zeno@albisser.org>
2007-10-28 16:13:05 +0000

    moved Security Tests to tests dir

---
+++ coast/modules/Security/Test/Base64WDRendererTest.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2005, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+#ifndef _Base64WDRendererTest_H
+#define _Base64WDRendererTest_H
+
+//---- baseclass include -------------------------------------------------
+#include "TestCase.h"
+
+//---- Base64WDRendererTest ----------------------------------------------------------
+//! <B>really brief class description</B>
+/*!
+further explanation of the purpose of the class
+this may contain <B>HTML-Tags</B>
+*/
+class Base64WDRendererTest : public TestCase
+{
+public:
+	//--- constructors
+
+	//! TestCase constructor
+	//! \param name name of the test
+	Base64WDRendererTest(TString name);
+
+	//! destroys the test case
+	~Base64WDRendererTest();
+
+	//--- public api
+
+	//! builds up a suite of testcases for this test
+	static Test *suite ();
+
+	//! sets the environment for this test
+	void setUp ();
+
+	//! deletes the environment for this test
+	void tearDown ();
+
+	//! describe this testcase
+	void RenderAllTest();
+};
+
+#endif
