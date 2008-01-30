69f34af3df2dda3848b354b522fef575cfb24d71
Marcel Huber <marcel.huber@hsr.ch>
2008-01-30 17:00:54 +0000

    mapper to put its output into an appropriate logfile

---
+++ coast/modules/AppLog/LoggingMapper.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2008, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+//--- interface include --------------------------------------------------------
+#include "LoggingMapper.h"
+
+//--- project modules used -----------------------------------------------------
+
+//--- standard modules used ----------------------------------------------------
+#include "AppLog.h"
+#include "Dbg.h"
+
+//--- c-modules used -----------------------------------------------------------
+
+//---- LoggingMapper ------------------------------------------------------------------
+RegisterResultMapper(LoggingMapper);
+
+LoggingMapper::LoggingMapper(const char *name)
+	: ResultMapper(name)
+{
+	StartTrace(LoggingMapper.Ctor);
+}
+
+IFAObject *LoggingMapper::Clone() const
+{
+	return new LoggingMapper(fName);
+}
+
+bool LoggingMapper::DoPutAny(const char *key, Anything value, Context &ctx, ROAnything script)
+{
+	StartTrace1(LoggingMapper.DoPutAny, NotNull(key));
+	String channel( Lookup("Channel", "") );
+	Trace("Channel: <" << channel << ">");
+	if ( channel.Length() ) {
+		TraceAny(value, "value for key <" << key << ">");
+		Context::PushPopEntry<ROAnything> aEntry(ctx, "LoggingMapperData", value, key);
+		return AppLogModule::Log(ctx, channel, (AppLogModule::eLogLevel)Lookup("Severity").AsLong((long)AppLogModule::eINFO));
+	}
+	return false;
+}
---
+++ coast/modules/AppLog/LoggingMapper.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2008, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+#ifndef _LoggingMapper_H
+#define _LoggingMapper_H
+
+//---- ResultMapper include -------------------------------------------------
+#include "Mapper.h"
+
+//---- LoggingMapper ----------------------------------------------------------
+//! <b>Mapper which logs messages on a configurable channel and routes message again</b>
+/*!
+<b>Configuration:</b><pre>
+{
+	/Channel		String		mandatory, channel name to log to
+	/Severity		long		optional, default AppLogModule::eINFO, Severity [CRITICAL=1, FATAL=2, ERROR=4, WARN=8, INFO=16, OK=32, MAINT=64, DEBUG=128], all levels lower_equal (<=) the specified value will get logged
+}</pre>
+*/
+class LoggingMapper : public ResultMapper
+{
+public:
+	//--- constructors
+	LoggingMapper(const char *name);
+	//--- support for prototype
+	IFAObject *Clone() const;
+
+protected:
+	//! Major hook for subclasses that want to do something with their config passed as script. The default is to interpret the script and put a value for every script item used. Recursion will be stopped by DoFinalPutAny which places its value under slot key below given DoGetDestinationSlot()
+	/*! \param key the key usually defines the associated kind of output-value
+		\param value the value to be mapped
+		\param ctx the context of the invocation
+		\param script current mapper configuration as ROAnything
+		\return returns true if the mapping was successful otherwise false */
+	virtual bool DoPutAny(const char *key, Anything value, Context &ctx, ROAnything script);
+
+private:
+	LoggingMapper();
+	LoggingMapper(const LoggingMapper &);
+	LoggingMapper &operator=(const LoggingMapper &);
+};
+
+#endif
