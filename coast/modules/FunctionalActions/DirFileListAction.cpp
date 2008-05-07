24afc1fac4fc197d55c7510df9bf38deabfd933b
Marcel Huber <marcel.huber@hsr.ch>
2008-05-07 12:24:50 +0000

    DirFileListAction added  - refactored from HIKU_DL and DCD

---
+++ coast/modules/FunctionalActions/DirFileListAction.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2008, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+//--- interface include --------------------------------------------------------
+#include "DirFileListAction.h"
+
+//--- project modules used -----------------------------------------------------
+
+//--- standard modules used ----------------------------------------------------
+#include "System.h"
+#include "Renderer.h"
+#include "Context.h"
+#include "Dbg.h"
+#include "Timers.h"
+
+//--- c-modules used -----------------------------------------------------------
+
+//---- DirFileListAction ---------------------------------------------------------------
+RegisterAction(DirFileListAction);
+
+DirFileListAction::DirFileListAction(const char *name) : Action(name) { }
+
+DirFileListAction::~DirFileListAction() { }
+
+bool DirFileListAction::DoExecAction(String &transitionToken, Context &ctx, const ROAnything &config)
+{
+	// this is the new method that also gets a config ( similar to Renderer::RenderAll )
+	// write the action code here - you don't have to override DoAction anymore
+	StartTrace(DirFileListAction.DoExecAction);
+	String sPath, sFilter, sTarget, sSort;
+
+	// Check Config if Path is defined cause mandatory
+	if ( config.IsDefined("Path") ) {
+		// Render slot
+		Renderer::RenderOnString(sPath, ctx, config["Path"]);
+		Trace("rendered sPath [" << sPath << "]");
+		if ( !sPath.Length() ) {
+			Trace("mandatory slot Path is empty after rendering !");
+			return false;
+		}
+	} else {
+		Trace("mandatory slot Path is missing in Config !");
+		return false;
+	}
+	// Check Config if Filter is defined
+	if ( config.IsDefined("Filter") ) {
+		sFilter = config["Filter"].AsString();
+		Trace("optional slot Filter is [" << sFilter << "]");
+	}
+	// Check Config if Target is defined
+	if ( config.IsDefined("Target") ) {
+		sTarget = config["Target"].AsString();
+		Trace("optional slot Target is [" << sTarget << "]");
+	}
+
+	Anything aTmpStore(ctx.GetTmpStore());
+	// Check if path is a valid path
+	if ( !System::IsDirectory(sPath) ) {
+		String strMsg("Slot Path contains not a valid path !");
+		Trace(strMsg);
+		aTmpStore["Error"] = strMsg;
+		return false;
+	}
+
+	// Do the Action
+	Anything aFileList;
+	{
+		MethodTimerUnit(DirFileListAction.DoExecAction, "GetDirFileList", ctx, TimeLogger::eMicroseconds);
+		aFileList = System::DirFileList(sPath, sFilter);
+	}
+	SubTraceAny(TraceList, aFileList, "Found [" << aFileList.GetSize() << "] files");
+
+	// Check Config if Sort is defined
+	if (config.IsDefined("Sort")) {
+		sSort = config["Sort"].AsString();
+		Trace("optional slot Sort is [" << sSort << "]");
+		MethodTimerUnit(DirFileListAction.DoExecAction, "SortList", ctx, TimeLogger::eMicroseconds);
+//XXX
+		aFileList.SortByStringValues();
+	}
+
+	if ( config.IsDefined("DirsOnly") ) {
+		bool bDirs( config["DirsOnly"].AsBool(false) );
+		Trace("optional slot DirsOnly is [" << bDirs << "]");
+		long ii( aFileList.GetSize() - 1 );
+		while (ii >= 0) {
+			Trace ("File to check [" << aFileList[ii].AsString() << "]");
+			String sCurrPath(sPath);
+			if ( sPath[sPath.Length()-1] != System::Sep() ) {
+				sCurrPath << System::Sep();
+			}
+			sCurrPath << aFileList[ii].AsString();
+			Trace ("Path to check [" << sCurrPath << "]");
+			if ( System::IsDirectory(sCurrPath) ) {
+				Trace ("File to check [" << aFileList[ii].AsString() << "] is Directory !!");
+				if ( !bDirs ) {
+					Trace("Removing Directory [" << aFileList[ii].AsString() << "] from list");
+					aFileList.Remove(ii);
+					++ii;
+				}
+			} else {
+				if ( bDirs ) {
+					Trace("Removing File [" << aFileList[ii].AsString() << "] from list");
+					aFileList.Remove(ii);
+					++ii;
+				}
+			}
+			--ii;
+		}
+	}
+
+	if ( sTarget.Length() ) {
+		aTmpStore[sTarget] = aFileList;
+	} else {
+		aTmpStore["RetrievedFileList"] = aFileList;
+	}
+	SubTraceAny(TraceListFinal, aFileList, "Found [" << aFileList.GetSize() << "] files");
+	return true;
+}
---
+++ coast/modules/FunctionalActions/DirFileListAction.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2008, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+#ifndef _DirFileListAction_H
+#define _DirFileListAction_H
+
+//---- Action include -------------------------------------------------
+#include "config_FunctionalActions.h"
+#include "Action.h"
+
+//---- DirFileListAction ----------------------------------------------------------
+//! <B>Retrieve directory entries at given Path and put results in context</B>
+/*!
+<B>Configuration:</B><PRE>
+{
+	/Path		Rendererspec	mandatory, path to list files of
+	/Filter		String			optional, used to filter the files listed
+	/DirsOnly 	Long			optional, [*0|1], 0 (default): files and directories are listed, 1: only directories
+	/Target 	Rendererspec	optional, default "RetrievedFileList", puts the list into the renderered slotname in context
+
+}</PRE>
+*/
+class EXPORTDECL_FUNCTIONALACTIONS DirFileListAction : public Action
+{
+public:
+	//--- constructors
+	/*! \param name defines the name of the action */
+	DirFileListAction(const char *name);
+	~DirFileListAction();
+
+	/*! DoSomething method for configured Actions
+		\param transitionToken (in/out) the event passed by the caller, can be modified.
+		\param ctx the context the action runs within.
+		\param config the configuration of the action.
+		\return true if the action run successfully, false if an error occurred.
+	*/
+	virtual bool DoExecAction(String &transitionToken, Context &ctx, const ROAnything &config);
+};
+
+#endif
