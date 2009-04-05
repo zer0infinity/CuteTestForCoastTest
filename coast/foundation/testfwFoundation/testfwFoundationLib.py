2bbfc9fd0941f607b11b744d50f0ad89a79a8042
Marcel Huber <marcel.huber@hsr.ch>
2009-04-05 22:58:42 +0200

    code to try finding dir of file

--- coast/foundation/testfwFoundation/testfwFoundationLib.py
+++ coast/foundation/testfwFoundation/testfwFoundationLib.py
@@ -1,7 +1,14 @@
-import os,platform
+import os,platform,pdb
+import SCons
 
 def generate(env, **kw):
     env.Tool('testfwLib')
+    tpath=kw.get('tpath', '')
+    if tpath != '':
+        pdb.set_trace()
+#        tinc=SCons.Script.Dir(tpath)
+#        print 'tinc [%s]' % tinc.path
+    env.AppendUnique(CPPPATH = ['#include/coast_testfw'])
 
 def exists(env):
     return true
