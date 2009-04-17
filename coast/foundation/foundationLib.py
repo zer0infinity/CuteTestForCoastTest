0b501159bf1c40fd88cf83727fdb7161f9933329
Marcel Huber <m1huber@hsr.ch>
2009-04-17 18:07:50 +0200

    added missing libraries for sun-gcc compilation used hardcoded path to gcc in /opt/brz/gcc/bin

--- coast/foundation/foundationLib.py
+++ coast/foundation/foundationLib.py
@@ -7,6 +7,9 @@ def generate(env, **kw):
 #    env.Tool('addLibrary', library = env['boostLibs'])
     # flags / settings used by this library and users of it
 #    env.AppendUnique(CPPDEFINES =['ONLY_STD_IOSTREAM'])
+    #FIXME: is this lib also needed when using sun-CC or only when using gcc?
+    if env['PLATFORM'] == "sunos":
+        env.AppendUnique(LIBS=['socket'])
     # export library dependency when we are not building ourselves
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['foundation'])
--- coast/mtfoundation/mtfoundationLib.py
+++ coast/mtfoundation/mtfoundationLib.py
@@ -9,6 +9,9 @@ def generate(env, **kw):
     env.AppendUnique(CPPDEFINES =['_POSIX_PTHREAD_SEMANTICS'])
     env.AppendUnique(CPPDEFINES =['_REENTRANT'])
     env.AppendUnique(LIBS=['pthread'])
+    #FIXME: is this lib also needed when using sun-CC or only when using gcc?
+    if env['PLATFORM'] == "sunos":
+        env.AppendUnique(LIBS=['posix4'])
     # export library dependency when we are not building ourselves
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['mtfoundation'])
