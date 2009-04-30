4ec3336a364a190b3dc1903991d8aed20d5dff24
Marcel Huber <m1huber@hsr.ch>
2009-04-30 23:02:29 +0200

    suncc-ified

--- coast/compress/ZipStream.cpp
+++ coast/compress/ZipStream.cpp
@@ -16,7 +16,11 @@
 #include "SysLog.h"
 
 //--- c-modules used -----------------------------------------------------------
+#if defined(__SUNPRO_CC)
+#include <strings.h>	// memset
+#else
 #include <cstring>	// memset
+#endif
 #include "gzio.h"	// DEF_MEM_LEVEL
 
 // following section copied out of zutil.h
