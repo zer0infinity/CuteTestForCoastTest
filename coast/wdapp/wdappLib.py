853b08532b2bdbd327e98b004f503ed79a1fd544
Marcel Huber <m1huber@hsr.ch>
2009-05-26 18:15:07 +0200

    rearranged and simplified usage of wdapp

--- coast/wdapp/wdappLib.py
+++ coast/wdapp/wdappLib.py
@@ -3,21 +3,23 @@ import SCons
 import StanfordUtils
 from SCons.Script import Dir
 
-_added = None
-_packagename = None
-_includeSubdir = ''
+_packagename = string.replace(__name__, 'Lib', '')
+_target = None
 
 def generate(env, **kw):
-    global _added, _packagename, _includeSubdir
-    if not _added:
-        _added = 1
-        _packagename = string.replace(__name__, 'Lib', '')
+    global _target, _packagename
 
     # this libraries dependencies
     StanfordUtils.DependsOn(env, 'CoastWDBase')
 
     # flags / settings used by this library and users of it
     # export library dependency when we are not building ourselves
+    return _target
 
 def exists(env):
     return true
+
+progEnv = StanfordUtils.CloneBaseEnv()
+StanfordUtils.DependsOn(progEnv, _packagename)
+
+_target = progEnv.Program(_packagename, StanfordUtils.listFiles(['Main.cpp']))
