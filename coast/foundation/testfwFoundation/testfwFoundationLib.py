ed3e3e4ea0d0519ac79531dd1933acf036baa285
Marcel Huber <m1huber@hsr.ch>
2009-05-28 16:37:09 +0200

    changed testfwFoundation to use new build mode

--- coast/foundation/testfwFoundation/testfwFoundationLib.py
+++ coast/foundation/testfwFoundation/testfwFoundationLib.py
@@ -1,47 +1,27 @@
 import os, pdb
 import StanfordUtils
 
-_packagename = StanfordUtils.getPackageName(__name__)
-_target = None
-_includeSubdir = ''
-_libDepends = ['testfw']
+packagename = StanfordUtils.getPackageName(__name__)
+target = None
 
-def generate(env, **kw):
-    global _target, _packagename, _includeSubdir, _libDepends
+buildSettings = {
+                 packagename : {
+                     'includeSubdir'    : '',
+                     'libDepends'       : ['testfw'],
+                     'sourceFiles'      : StanfordUtils.listFiles(['*.cpp']),
+                     'publicIncludes'   : StanfordUtils.listFiles([os.path.join('', '*.h')]),
+#                     'publicIncludes'   : StanfordUtils.listFiles([os.path.join(buildSettings.get('includeSubdir',''), '*.h')]),
+#                     'appendUnique'     : { 'CPPDEFINES' : 'fooX' },
+                 }
+                }
 
-    # this libraries dependencies
-    StanfordUtils.setModuleDependencies(env, _libDepends)
-
-    if _target:
-        env.Tool('addLibrary', library=[_packagename])
-
-    # flags / settings used by this library and users of it
-#    env.AppendUnique(CPPDEFINES =['SOMEFLAG'])
-
-    # specify public headers here
-    StanfordUtils.setIncludePath(env, _packagename, _includeSubdir, internal=False)
-
-    return _target
-
-# create environment for target
-targetEnv = StanfordUtils.CloneBaseEnv()
-
-# update environment by adding dependencies to used modules
-StanfordUtils.setModuleDependencies(targetEnv, _libDepends)
-
-# win32 specific define to export all symbols when creating a DLL
-##targetEnv.AppendUnique(CPPDEFINES=[_packagename.upper() + '_IMPL'])
-
-# maybe we need to add this libraries local include path when building it (if different from .)
-StanfordUtils.setIncludePath(targetEnv, _packagename, _includeSubdir)
-
-# specify this modules target
-##_target = targetEnv.SharedLibrary(_packagename, StanfordUtils.listFiles(['*.cpp']))
+envVars = StanfordUtils.EnvVarDict(CPPDEFINES=[packagename.upper() + '_IMPL'])
+targetEnv, target = StanfordUtils.createSharedLibrary(packagename, buildSettings, envVars=envVars)
 
 # either create a new environment to register this package with or use targetEnv when no real target gets created
 registerEnv = targetEnv
 registerEnv.Tool('registerObjects',
-             package=_packagename,
-             libraries=[_target],
-             includes=StanfordUtils.listFiles([os.path.join(_includeSubdir, '*.h')])
+             package=packagename,
+             libraries=[target],
+             includes=buildSettings[packagename].get('publicIncludes', [])
              )
