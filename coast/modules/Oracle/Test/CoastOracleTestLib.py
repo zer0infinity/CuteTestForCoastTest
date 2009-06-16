09f57faee609757adfbcaaa247c4076cc88cad53
Marcel Huber <m1huber@hsr.ch>
2009-06-16 18:17:04 +0200

    added initial set of testcases for oracle

---
+++ coast/modules/Oracle/Test/CoastOracleTestLib.py
@@ -0,0 +1,18 @@
+import StanfordUtils
+
+packagename = StanfordUtils.getPackageName(__name__)
+
+buildSettings = {
+                 packagename : {
+                     'targetType'       : 'ProgramTest',
+                     'linkDependencies' : ['CoastOracle', 'testfwWDBase'],
+                     'requires'         : ['CoastAppLog', 'CoastRenderers'],
+                     'sourceFiles'      : StanfordUtils.listFiles(['*.cpp']),
+                     'configFiles'      : StanfordUtils.listFiles(['config/*.any',
+                                                                   'config/*.txt',
+                                                                   'config/*.tst',
+                                                                   'tmp/*.any']),
+                     },
+                 }
+
+StanfordUtils.createTargets(packagename, buildSettings)
---
+++ coast/modules/Oracle/Test/SetupRunner.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2009, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+ * All rights reserved.
+ *
+ * This library/application is free software; you can redistribute and/or modify it under the terms of
+ * the license that is included with this library/application in the file license.txt.
+ */
+
+//--- interface include --------------------------------------------------------
+#include "TestRunner.h"
+
+//--- test cases ---------------------------------------------------------------
+#include "ConfiguredActionTest.h"
+
+void setupRunner(TestRunner &runner)
+{
+	// execute tests in test dir with defined structure
+	ADD_SUITE(runner, ConfiguredActionTest);
+}
