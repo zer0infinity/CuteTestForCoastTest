54bff9447e3b7e033c33da62ca71475c2854b426
Marcel Huber <m1huber@hsr.ch>
2009-06-17 08:57:58 +0200

    added ociicus as required target because we should not link against

--- coast/modules/Oracle/CoastOracleLib.py
+++ coast/modules/Oracle/CoastOracleLib.py
@@ -6,6 +6,7 @@ packagename = StanfordUtils.getPackageName(__name__)
 buildSettings = {
                  packagename : {
                      'linkDependencies' : ['oracle', 'CoastDataAccess'],
+                     'requires'         : ['oracle.ociicus'],
                      'sourceFiles'      : StanfordUtils.listFiles(['*.cpp']),
                      'targetType'       : 'LibraryShared',
                      'appendUnique'     : { 'CPPDEFINES' : [packagename.upper() + '_IMPL'] },
