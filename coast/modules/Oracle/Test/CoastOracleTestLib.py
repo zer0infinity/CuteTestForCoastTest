f56cd52cfcb2841ab2323ddee46bb1cc531184ac
Marcel Huber <m1huber@hsr.ch>
2009-07-08 10:35:15 +0200

    dir variable cleanup adjusted path names in RunBuilder creating logdir/files when build setup produces stderr/stdout output

--- coast/modules/Oracle/Test/CoastOracleTestLib.py
+++ coast/modules/Oracle/Test/CoastOracleTestLib.py
@@ -1,29 +1,46 @@
 from __future__ import with_statement
-import os, subprocess
+import os, subprocess, pdb
 import StanfordUtils
 
 packagename = StanfordUtils.getPackageName(__name__)
 
-def runSQL(connectstring, filename):
+def runSQL(connectstring, filename, logpath):
     targets = StanfordUtils.programLookup.getPackageTarget('oracle', 'sqlplus')
-
-    if not targets['target'] or not os.path.isfile(filename):
+    target = targets['target']
+    if not target or not os.path.isfile(filename):
         return False
-
-    args = [targets['target'].abspath, connectstring]
+    args = [target.abspath, connectstring]
     res = 0
+    logfilebasename = target.name + '.' + os.path.basename(filename)
     with open(filename) as file:
-        res = subprocess.call(args, stdin=file)
+        proc = subprocess.Popen(args, stdin=file,
+                                     stderr=subprocess.PIPE,
+                                     stdout=subprocess.PIPE)
+        res = proc.wait()
+        perr = proc.stderr.read()
+        pout = proc.stdout.read()
+
+        if not os.path.isdir(logpath):
+            os.makedirs(logpath)
+        if perr:
+            with open(os.path.join(logpath, logfilebasename + '.stderr'), 'w') as errfile:
+                errfile.write(perr)
+        if pout:
+            with open(os.path.join(logpath, logfilebasename + '.stdout'), 'w') as outfile:
+                outfile.write(pout)
+
     return res
 
 def setUp(target, source, env):
-    path = StanfordUtils.programLookup.getPackageDir(packagename).Dir('config')
-    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('drop_oratest_schema.sql').abspath)
-    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('oratest_schema.sql').abspath)
+    logpath = env['BASEOUTDIR'].Dir(os.path.join('tests', packagename, env['LOGDIR'], env['VARIANTDIR']))
+    path = env['BASEOUTDIR'].Dir(os.path.join('tests', packagename)).Dir('config')
+    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('drop_oratest_schema.sql').abspath, logpath.abspath)
+    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('oratest_schema.sql').abspath, logpath.abspath)
 
 def tearDown(target, source, env):
-    path = StanfordUtils.programLookup.getPackageDir(packagename).Dir('config')
-    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('drop_oratest_schema.sql').abspath)
+    logpath = env['BASEOUTDIR'].Dir(os.path.join('tests', packagename, env['LOGDIR'], env['VARIANTDIR']))
+    path = env['BASEOUTDIR'].Dir(os.path.join('tests', packagename)).Dir('config')
+    runSQL("oratest/oratest@sifs-coast1.hsr.ch:1521/orcl", path.File('drop_oratest_schema.sql').abspath, logpath.abspath)
 
 buildSettings = {
                  packagename : {
