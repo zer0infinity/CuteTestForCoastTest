e90ad0bada103cf42d4d72da6a3dc7d939809ecd
Marcel Huber <marcel.huber@hsr.ch>
2009-12-22 23:08:03 +0100

    corrected replaceRegexInFile call

--- coast/foundation/Test/CoastFoundationTestLib.py
+++ coast/foundation/Test/CoastFoundationTestLib.py
@@ -3,80 +3,79 @@ import StanfordUtils
 import SocketHelpers
 from stat import *
 
-packagename = StanfordUtils.getPackageName( __name__ )
+packagename = StanfordUtils.getPackageName(__name__)
 acceptorThreads = []
 
-def acceptorCallback( sock, addr ):
-    data = SocketHelpers.sockrecv( sock )
-    sock.send( '' )
-    sock.shutdown( socket.SHUT_RDWR )
+def acceptorCallback(sock, addr):
+    data = SocketHelpers.sockrecv(sock)
+    sock.send('')
+    sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
-def httpReplyCallback( sock, addr ):
-    data = SocketHelpers.sockrecv( sock )
-    sock.send( "HTTP/1.1 200 OK\n\n" )
-    sock.shutdown( socket.SHUT_RDWR )
+def httpReplyCallback(sock, addr):
+    data = SocketHelpers.sockrecv(sock)
+    sock.send("HTTP/1.1 200 OK\n\n")
+    sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
-def noSendRecvCallback( sock, addr ):
-    sock.shutdown( socket.SHUT_RDWR )
+def noSendRecvCallback(sock, addr):
+    sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
 basepath = ''
 
-def SystemTestModifyConfig( env, cfgfilename ):
+def SystemTestModifyConfig(env, cfgfilename):
     global basepath
-    basepath = os.path.join( os.path.join( env['BASEOUTDIR'].Dir( env['RELTARGETDIR'] ).abspath, 'makedirextendtest' ), 'test' )
-    tdirabs = env['BASEOUTDIR'].Dir( env['RELTARGETDIR'] ).abspath
-    fname = os.path.join( tdirabs, cfgfilename )
+    basepath = os.path.join(os.path.join(env['BASEOUTDIR'].Dir(env['RELTARGETDIR']).abspath, 'makedirextendtest'), 'test')
+    tdirabs = env['BASEOUTDIR'].Dir(env['RELTARGETDIR']).abspath
+    fname = os.path.join(tdirabs, cfgfilename)
     ## set writable cfgfilename
-    if os.path.isfile( fname ):
-        os.chmod( fname, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH )
+    if os.path.isfile(fname):
+        os.chmod(fname, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
         ## replace some token in Tests configuration file to store number of existing directories so far
         ## the number of existing dirs is dirnumber + 2 ('.' and '..')
-        StanfordUtils.replaceRegexInFile( fname, "##DIREXTENDBASEDIR##", basepath )
-        if str( env['PLATFORM'] ) == 'cygwin':
-            StanfordUtils.replaceRegexInFile( fname, "##TMPDIR##", 'c:/temp' )
-            StanfordUtils.replaceRegexInFile( fname, "##ROOTFS##", 'c:/' )
+        searchReplace = [(r"##DIREXTENDBASEDIR##", basepath) ]
+        if str(env['PLATFORM']) == 'cygwin':
+            searchReplace.extend([ (r"##TMPDIR##", 'c:/temp'), (r"##ROOTFS##", 'c:/') ])
         else:
-            StanfordUtils.replaceRegexInFile( fname, "##TMPDIR##", '/tmp' )
-            StanfordUtils.replaceRegexInFile( fname, "##ROOTFS##", '/' )
+            searchReplace.extend([ (r"##TMPDIR##", '/tmp'), (r"##ROOTFS##", '/') ])
+        StanfordUtils.replaceRegexInFile(fname, searchReplace)
 
 def SystemTestMakeDirectoryExtendCleanup():
     global basepath
-    for p in [basepath + '_ex0', basepath, os.path.split( basepath )[0]]:
-        if os.path.isdir( p ):
+    for p in [basepath + '_ex0', basepath, os.path.split(basepath)[0]]:
+        if os.path.isdir(p):
             try:
-                os.rmdir( p )
+                os.rmdir(p)
             except: pass
 
-def setUp( target, source, env ):
-    SystemTestModifyConfig( env, 'config/SystemTest.any' )
+def setUp(target, source, env):
+    SystemTestModifyConfig(env, 'config/SystemTest.any')
     # setup listener threads for socket tests
     global acceptorThreads
-    acceptorThreads.append( SocketHelpers.AcceptorThread( addr = '', port = 9876, callback = acceptorCallback ) )
-    acceptorThreads.append( SocketHelpers.AcceptorThread( addr = '', port = 9875, callback = httpReplyCallback ) )
-    acceptorThreads.append( SocketHelpers.AcceptorThread( addr = '', port = 9874, callback = noSendRecvCallback ) )
+    acceptorThreads.append(SocketHelpers.AcceptorThread(addr='', port=9876, callback=acceptorCallback))
+    acceptorThreads.append(SocketHelpers.AcceptorThread(addr='', port=9875, callback=httpReplyCallback))
+    acceptorThreads.append(SocketHelpers.AcceptorThread(addr='', port=9874, callback=noSendRecvCallback))
     for t in acceptorThreads:
         t.start()
 
-def tearDown( target, source, env ):
+def tearDown(target, source, env):
     global acceptorThreads
     for t in acceptorThreads:
         if t.isAlive():
             t.terminate()
             t.join()
     # delete generated files
-    StanfordUtils.removeFiles( StanfordUtils.findFiles( [env['BASEOUTDIR'].Dir( env['RELTARGETDIR'] )], extensions = ['.res', '.tst', '.txt' ], matchfiles = ['include.any'] ) )
+    StanfordUtils.removeFiles(StanfordUtils.findFiles([env['BASEOUTDIR'].Dir(env['RELTARGETDIR'])], extensions=['.res', '.tst', '.txt' ], matchfiles=['include.any']))
     SystemTestMakeDirectoryExtendCleanup()
 
 buildSettings = {
     packagename : {
         'linkDependencies' : ['CoastFoundation', 'testfwFoundation'],
-        'sourceFiles'      : StanfordUtils.listFiles( ['*.cpp'] ),
+        'sourceFiles'      : StanfordUtils.listFiles(['*.cpp']),
         'targetType'       : 'ProgramTest',
-        'copyFiles'        : [( StanfordUtils.findFiles( ['.'], ['.any', '.txt', '.tst'] ), S_IRUSR | S_IRGRP | S_IROTH ),
-                              ( StanfordUtils.findFiles( ['config'], ['.sh'] ), S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR )],
+        'copyFiles'        : [(StanfordUtils.findFiles(['.'], ['.any', '.txt', '.tst']), S_IRUSR | S_IRGRP | S_IROTH),
+                              (StanfordUtils.findFiles(['config'], ['.sh']), S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR)],
         'runConfig'        : {
                               'setUp': setUp,
                               'tearDown': tearDown,
@@ -84,4 +83,4 @@ buildSettings = {
     }
 }
 
-StanfordUtils.createTargets( packagename, buildSettings )
+StanfordUtils.createTargets(packagename, buildSettings)
