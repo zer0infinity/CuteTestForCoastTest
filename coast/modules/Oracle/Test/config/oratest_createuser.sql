9ef5de37b1bc74a298229050c9dc215ba7454d44
Marcel Huber <marcel.huber@hsr.ch>
2010-06-17 15:39:13 +0200

    factored out backend server configuration into Defaults.any
    added any references to values factored out

--- coast/modules/Oracle/Test/config/oratest_createuser.sql
+++
@@ -1,9 +0,0 @@
-drop user oratest cascade;
-create user oratest identified by oratest;
-grant connect to oratest;
-grant resource to oratest;
-
-drop user oratest2 cascade;
-create user oratest2 identified by oratest2;
-grant connect to oratest2;
-grant CREATE SYNONYM to oratest2;
