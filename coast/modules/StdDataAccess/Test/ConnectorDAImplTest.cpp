fef95412eb2ff5444c60b226d47607e1390a51b7
Marcel Huber <marcel.huber@hsr.ch>
2010-06-24 11:00:24 +0200

    factored out hardcoded host/port settings into Defaults.any

--- coast/modules/StdDataAccess/Test/ConnectorDAImplTest.cpp
+++ coast/modules/StdDataAccess/Test/ConnectorDAImplTest.cpp
@@ -33,82 +33,140 @@ using namespace std;
 #include <sys/socket.h>		// for SO_ERROR
 #endif
 
-class AcceptorThread : public Thread
-{
+class EchoMsgCallBack: public AcceptorCallBack {
 public:
-	AcceptorThread(Acceptor *acceptor) : Thread("AcceptorThread"), fAcceptor(acceptor)
-	{ }
-	~AcceptorThread();
-	void Run();
+	//!default constructor is empty since class provides only protocol no implementation
+	EchoMsgCallBack() :
+		fReplyMessage("Accepted") {
+	}
+
+	//!default destructor is empty since class provides only protocol no implementation
+	virtual ~EchoMsgCallBack() {
+	}
+
+	//!call back functionality provides the socket object
+	//! \param socket the Socket object resulting from the accepted connection
+	virtual void CallBack(Socket *pSocket) {
+		StartTrace(EchoMsgCallBack.CallBack);
+		if (pSocket != 0) { // might be 0 if we run out of memory, not likely
+			while (true) {
+				iostream *Ios = pSocket->GetStream();
+				if (Ios != 0 && (*Ios)) {
+					TimeoutModifier aTimeoutModifier((SocketStream *) Ios, 10 * 1000);
+					aTimeoutModifier.Use();
+					Trace("stream valid");
+					// get character from stream, blocking call
+					int ch = Ios->get();
+					if ((ch == EOF) || !Ios->good()) {
+						SYSINFO("termination requested due to closed or bad stream");
+						break;
+					}
+					// put back character to allow reading of whole message
+					Ios->putback(ch);
+					Trace("First character from stream " << String("0x").AppendAsHex(ch) << ":[" << (char)ch << "]");
+
+					Anything anyMessage;
+					anyMessage.Import(*Ios, "importing stream");
+					TraceAny(anyMessage, "Message");
+					fLastRequest = anyMessage;
+					long lRetCode;
+					if (pSocket->IsReadyForWriting(10 * 1000, lRetCode)) {
+						Trace("sending back reply message");
+						fReplyMessage.PrintOn(*Ios, false) << flush;
+					} else {
+						SYSWARNING("socket not ready for writing!");
+					}
+					if (anyMessage.IsDefined("CloseConnection") && anyMessage["CloseConnection"].AsBool(false)) {
+						Trace("closing socket");
+						break;
+					}
+				}
+			}
+			pSocket->ShutDown();
+		}
+		delete pSocket;
+	}
+
+	//!I return the last message I read
+	virtual Anything &GetLastRequest() {
+		StartTrace(EchoMsgCallBack.GetLastRequest);
+		return fLastRequest;
+	}
+
+	//!Set the message that I shall return on subsequent requests
+	void SetReplyMessage(Anything &reply) {
+		StartTrace(EchoMsgCallBack.SetReplyMessage);
+		fReplyMessage = reply;
+	}
 
 protected:
-	bool DoStartRequestedHook(ROAnything args);
-	void DoTerminationRequestHook(ROAnything);
+	// no locking as only one thread initiates actions and the second thread
+	// only accesses while the first one waits for I/O
+	Anything fLastRequest;
+	Anything fReplyMessage;
+};
+
+class AcceptorThread: public Thread {
+public:
+	AcceptorThread(Acceptor *acceptor) :
+		Thread("AcceptorThread"), fAcceptor(acceptor) {
+	}
+	~AcceptorThread() {
+		StartTrace(AcceptorThread.Dtor);
+		if (fAcceptor) {
+			delete fAcceptor;
+		}
+		fAcceptor = 0;
+	}
+	void Run() {
+		StartTrace(AcceptorThread.Run);
+		if (fAcceptor) {
+			fAcceptor->RunAcceptLoop();
+		}
+	}
+
+protected:
+	bool DoStartRequestedHook(ROAnything args) {
+		StartTrace(AcceptorThread.DoStartRequestedHook);
+		if (fAcceptor) {
+			fAcceptor->SetThreadLocal(true);
+			return (fAcceptor->PrepareAcceptLoop() == 0);
+		}
+		return false;
+	}
+	void DoTerminationRequestHook(ROAnything) {
+		StartTrace(AcceptorThread.DoTerminationRequestHook);
+		if (fAcceptor) {
+			fAcceptor->StopAcceptLoop();
+		}
+	}
 
 private:
 	Acceptor *fAcceptor;
 };
 
-class EchoMsgCallBack : public AcceptorCallBack
-{
-public:
-
-	//!default constructor is empty since class provides only protocol no implementation
-	EchoMsgCallBack() : fReplyMessage("Accepted") {};
-
-	//!default destructor is empty since class provides only protocol no implementation
-	virtual ~EchoMsgCallBack() {};
-
-	//!call back functionality provides the socket object
-	//! \param socket the Socket object resulting from the accepted connection
-	virtual void CallBack(Socket *socket);
-
-	//!I return the last message I read
-	virtual Anything &GetLastRequest();
-
-	//!Set the message that I shall return on subsequent requests
-	void SetReplyMessage(Anything &reply);
-
-protected:
-	// no locking as only one thread initiates actions and the second thread
-	// only accesses while the first one waits for I/O
-	Anything	fLastRequest;
-	Anything	fReplyMessage;
-};
-
 //---- ConnectorDAImplTest ----------------------------------------------------------------
-ConnectorDAImplTest::ConnectorDAImplTest(TString strName)
-	: TestCaseType(strName)
-	, fCallBack(0)
-	, fAcceptorThread(0)
-{
+ConnectorDAImplTest::ConnectorDAImplTest(TString strName) :
+	TestCaseType(strName), fCallBack(0), fAcceptorThread(0) {
 	StartTrace(ConnectorDAImplTest.ConnectorDAImplTest);
 }
 
-TString ConnectorDAImplTest::getConfigFileName()
-{
+TString ConnectorDAImplTest::getConfigFileName() {
 	return "ConnectorDAImplTestConfig";
 }
 
-ConnectorDAImplTest::~ConnectorDAImplTest()
-{
+ConnectorDAImplTest::~ConnectorDAImplTest() {
 	StartTrace(ConnectorDAImplTest.Dtor);
 }
 
-void ConnectorDAImplTest::setUp ()
-{
+void ConnectorDAImplTest::setUp() {
 	StartTrace(ConnectorDAImplTest.setUp);
-
 	TraceAny(GetTestCaseConfig(), "TestCaseConfig");
-	ROAnything roaConfig(GetTestCaseConfig());
-	String strHost = roaConfig["Address"].AsString();
-	long   lPort = roaConfig["Port"].AsLong();
-
 	fCallBack = new EchoMsgCallBack();
-	fAcceptorThread = new AcceptorThread(new Acceptor(strHost, lPort, 2, fCallBack));
+	fAcceptorThread = new AcceptorThread(new Acceptor(GetTestCaseConfig()["Address"].AsString(), GetTestCaseConfig()["Port"].AsLong(), 2, fCallBack));
 	t_assert(fCallBack != 0);
 	t_assert(fAcceptorThread != 0);
-	if ( (fCallBack) && (fAcceptorThread) ) {
+	if ((fCallBack) && (fAcceptorThread)) {
 		bool startsuccesful = fAcceptorThread->Start();
 		t_assertm(startsuccesful, "probably bind failed; since thread not started");
 		bool running = fAcceptorThread->CheckState(Thread::eRunning, 3);
@@ -126,10 +184,8 @@ void ConnectorDAImplTest::setUp ()
 	t_assertm(fAcceptorThread != 0, "expected AcceptorThread to be ready; init or startup failed");
 }
 
-void ConnectorDAImplTest::tearDown ()
-{
+void ConnectorDAImplTest::tearDown() {
 	StartTrace(ConnectorDAImplTest.tearDown);
-
 	if (fAcceptorThread) {
 		fAcceptorThread->Terminate();
 		t_assert(fAcceptorThread->CheckState(Thread::eTerminated, 3));
@@ -138,36 +194,34 @@ void ConnectorDAImplTest::tearDown ()
 	}
 }
 
-bool ConnectorDAImplTest::IsSocketValid(int socketFd)
-{
+bool ConnectorDAImplTest::IsSocketValid(int socketFd) {
 	StartTrace(ConnectorDAImplTest.IsSocketValid);
 	int error = 0;
 	bool bSuccess = Socket::GetSockOptInt(socketFd, SO_ERROR, error);
-	if ( !bSuccess ) {
+	if (!bSuccess) {
 		SYSWARNING("socketFd " << (long)socketFd << " not valid anymore, error:" << (long)error);
 		return false;
 	}
 	return true;
 }
 
-void ConnectorDAImplTest::SendReceiveOnceTest()
-{
+void ConnectorDAImplTest::SendReceiveOnceTest() {
 	StartTrace(ConnectorDAImplTest.SendReceiveOnceTest);
 	TraceAny(GetTestCaseConfig(), "TestCaseConfig");
 	Anything anyEnv = GetTestCaseConfig().DeepClone();
 	Context ctx(anyEnv);
 	DataAccess aDA("SendReceiveOnce");
-	t_assert(aDA.StdExec(ctx));
-	Anything anyExpected;
-	String strIn = GetTestCaseConfig()["Input"].AsString();
-	IStringStream stream(strIn);
-	anyExpected.Import(stream);
-	assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+	if ( t_assert(aDA.StdExec(ctx)) ) {
+		Anything anyExpected;
+		String strIn = GetTestCaseConfig()["Input"].AsString();
+		IStringStream stream(strIn);
+		anyExpected.Import(stream);
+		assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+	}
 	TraceAny(ctx.GetTmpStore(), "TmpStore");
 }
 
-void ConnectorDAImplTest::RecreateSocketTest()
-{
+void ConnectorDAImplTest::RecreateSocketTest() {
 	StartTrace(ConnectorDAImplTest.RecreateSocketTest);
 	TraceAny(GetTestCaseConfig(), "TestCaseConfig");
 	Anything anyEnv = GetTestCaseConfig().DeepClone();
@@ -175,34 +229,35 @@ void ConnectorDAImplTest::RecreateSocketTest()
 	{
 		DataAccess aDA("RecreateSocket");
 		ctx.GetTmpStore()["Input"] = "{ \"Kurt hat recht\" }";
-		t_assert(aDA.StdExec(ctx));
-		Anything anyExpected;
-		String strIn = ctx.GetTmpStore()["Input"].AsString();
-		IStringStream stream(strIn);
-		anyExpected.Import(stream);
-		assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+		if ( t_assert(aDA.StdExec(ctx)) ) {
+			Anything anyExpected;
+			String strIn = ctx.GetTmpStore()["Input"].AsString();
+			IStringStream stream(strIn);
+			anyExpected.Import(stream);
+			assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+		}
+		TraceAny(ctx.GetTmpStore(), "TmpStore");
 	}
 	// test if socketFd is still valid
 	int socketFd = ctx.GetTmpStore()["SocketParams"]["SocketFd"].AsLong(0L);
-	if ( assertCompare(0, not_equal_to, socketFd) ) {
+	if (assertCompare(0, not_equal_to, socketFd)) {
 		t_assertm(IsSocketValid(socketFd), "expected socket to be valid");
-		{
-			DataAccess aDA("SendReceiveOnce");
-			ctx.GetTmpStore()["Input"] = "{ \"Marcel auch\" }";
-			t_assert(aDA.StdExec(ctx));
+		DataAccess aDA("SendReceiveOnce");
+		ctx.GetTmpStore()["Input"] = "{ \"Marcel auch\" }";
+		if ( t_assert(aDA.StdExec(ctx)) ) {
 			Anything anyExpected;
 			String strIn(ctx.GetTmpStore()["Input"].AsString());
 			IStringStream stream(strIn);
 			anyExpected.Import(stream);
 			assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
 		}
+		TraceAny(ctx.GetTmpStore(), "TmpStore");
 	}
 	// test if socketFd is not valid any longer
 	t_assertm(!IsSocketValid(socketFd), "expected socket to be invalid");
 }
 
-void ConnectorDAImplTest::RecreateClosedSocketTest()
-{
+void ConnectorDAImplTest::RecreateClosedSocketTest() {
 	StartTrace(ConnectorDAImplTest.RecreateSocketTest);
 	TraceAny(GetTestCaseConfig(), "TestCaseConfig");
 	Anything anyEnv = GetTestCaseConfig().DeepClone();
@@ -210,147 +265,56 @@ void ConnectorDAImplTest::RecreateClosedSocketTest()
 	{
 		DataAccess aDA("RecreateSocket");
 		ctx.GetTmpStore()["Input"] = "{ \"Kurt hat recht\" }";
-		t_assert(aDA.StdExec(ctx));
-		Anything anyExpected;
-		String strIn = ctx.GetTmpStore()["Input"].AsString();
-		IStringStream stream(strIn);
-		anyExpected.Import(stream);
-		assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+		if ( t_assert(aDA.StdExec(ctx)) ) {
+			Anything anyExpected;
+			String strIn = ctx.GetTmpStore()["Input"].AsString();
+			IStringStream stream(strIn);
+			anyExpected.Import(stream);
+			assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
+		}
+		TraceAny(ctx.GetTmpStore(), "TmpStore");
 	}
 
 	// test if socketFd is still valid
 	int socketFd = ctx.GetTmpStore()["SocketParams"]["SocketFd"].AsLong(0L);
-	if ( assertCompare(0, not_equal_to, socketFd) ) {
+	if (assertCompare(0, not_equal_to, socketFd)) {
 		t_assertm(IsSocketValid(socketFd), "expected socket to be valid");
 		// force EchoMsgCallBack to close the socket from the server side
-		{
-			DataAccess aDA("RecreateSocket");
-			Anything anyMessage;
-			anyMessage["CloseConnection"] = 1;
-			ctx.GetTmpStore()["Input"] = anyMessage;
-			t_assert(aDA.StdExec(ctx));
+		DataAccess aDA("RecreateSocket");
+		Anything anyMessage;
+		anyMessage["CloseConnection"] = 1;
+		ctx.GetTmpStore()["Input"] = anyMessage;
+		if ( t_assert(aDA.StdExec(ctx)) ) {
 			assertAnyEqual(anyMessage, fCallBack->GetLastRequest());
 		}
+		TraceAny(ctx.GetTmpStore(), "TmpStore");
 	}
 
 	// although the socket is closed from the server side, the socketFd should still seem to be valid
 	socketFd = ctx.GetTmpStore()["SocketParams"]["SocketFd"].AsLong(0L);
-	if ( assertCompare(0, not_equal_to, socketFd) ) {
+	if (assertCompare(0, not_equal_to, socketFd)) {
 		t_assertm(IsSocketValid(socketFd), "expected socket to be valid");
-		{
-			DataAccess aDA("SendReceiveOnce");
-			ctx.GetTmpStore()["Input"] = "{ \"Marcel auch\" }";
-			t_assert(aDA.StdExec(ctx));
+		DataAccess aDA("SendReceiveOnce");
+		ctx.GetTmpStore()["Input"] = "{ \"Marcel auch\" }";
+		if ( t_assert(aDA.StdExec(ctx)) ) {
 			Anything anyExpected;
 			String strIn(ctx.GetTmpStore()["Input"].AsString());
 			IStringStream stream(strIn);
 			anyExpected.Import(stream);
 			assertAnyEqual(anyExpected, fCallBack->GetLastRequest());
 		}
+		TraceAny(ctx.GetTmpStore(), "TmpStore");
 	}
 	// test if socketFd is not valid any longer
 	t_assertm(!IsSocketValid(socketFd), "expected socket to be invalid");
 }
 
-Anything &EchoMsgCallBack::GetLastRequest()
-{
-	StartTrace(EchoMsgCallBack.GetLastRequest);
-	return fLastRequest;
-}
-
-void EchoMsgCallBack::SetReplyMessage( Anything &reply )
-{
-	StartTrace(EchoMsgCallBack.SetReplyMessage);
-	fReplyMessage = reply;
-}
-
-void EchoMsgCallBack::CallBack(Socket *pSocket)
-{
-	StartTrace(EchoMsgCallBack.CallBack);
-	if (pSocket != 0) {	// might be 0 if we run out of memory, not likely
-		while ( true ) {
-			iostream *Ios = pSocket->GetStream();
-			if ( Ios != 0 ) {
-				TimeoutModifier aTimeoutModifier((SocketStream *) Ios, 10 * 1000);
-				aTimeoutModifier.Use();
-				Trace("stream valid");
-				// get character from stream, blocking call
-				int ch = Ios->get();
-				if ( (ch == EOF) || !Ios->good() ) {
-					SYSINFO("termination requested due to closed or bad stream");
-					break;
-				}
-				// put back character to allow reading of whole message
-				Ios->putback(ch);
-				Trace("First character from stream " << String("0x").AppendAsHex(ch) << ":[" << (char)ch << "]");
-
-				Anything anyMessage;
-				(*Ios ) >> anyMessage;
-				TraceAny(anyMessage, "Message");
-				fLastRequest = anyMessage;
-				long lRetCode;
-				if ( pSocket->IsReadyForWriting(10 * 1000, lRetCode) ) {
-					Trace("sending back reply message");
-					fReplyMessage.PrintOn(*Ios, false) << flush;
-				} else {
-					SYSWARNING("socket not ready for writing!");
-				}
-				if ( anyMessage.IsDefined("CloseConnection") && anyMessage["CloseConnection"].AsBool(false) ) {
-					Trace("closing socket");
-					break;
-				}
-			}
-		}
-		pSocket->ShutDown();
-	}
-	delete pSocket;
-}
-
-// ---------- AcceptorThread ------------------------------
-AcceptorThread::~AcceptorThread()
-{
-	StartTrace(AcceptorThread.Dtor);
-	if (fAcceptor) {
-		delete fAcceptor;
-	}
-	fAcceptor = 0;
-}
-
-bool AcceptorThread::DoStartRequestedHook(ROAnything args)
-{
-	StartTrace(AcceptorThread.DoStartRequestedHook);
-	if (fAcceptor) {
-		fAcceptor->SetThreadLocal(true);
-		return (fAcceptor->PrepareAcceptLoop() == 0);
-	}
-	return false;
-}
-
-void AcceptorThread::Run()
-{
-	StartTrace(AcceptorThread.Run);
-	if (fAcceptor) {
-		fAcceptor->RunAcceptLoop();
-	}
-}
-
-void AcceptorThread::DoTerminationRequestHook(ROAnything)
-{
-	StartTrace(AcceptorThread.DoTerminationRequestHook);
-	if (fAcceptor) {
-		fAcceptor->StopAcceptLoop();
-	}
-}
-
 // builds up a suite of testcases, add a line for each testmethod
-Test *ConnectorDAImplTest::suite ()
-{
+Test *ConnectorDAImplTest::suite() {
 	StartTrace(ConnectorDAImplTest.suite);
 	TestSuite *testSuite = new TestSuite;
-
 	ADD_CASE(testSuite, ConnectorDAImplTest, SendReceiveOnceTest);
 	ADD_CASE(testSuite, ConnectorDAImplTest, RecreateSocketTest);
 	ADD_CASE(testSuite, ConnectorDAImplTest, RecreateClosedSocketTest);
-
 	return testSuite;
 }
