79d046e5ac8632b157cfc0280fb1409e8dbc2b85
Michael Rüegg <mrueegg@hsr.ch>
2010-09-09 14:41:14 +0200

    Fixed broken MySQL tests

--- coast/modules/MySQL/Test/config/TestDBDefinition.sql
+++ coast/modules/MySQL/Test/config/TestDBDefinition.sql
@@ -13,11 +13,3 @@ INSERT INTO TestTable VALUES ("Test",1);
 INSERT INTO TestTable VALUES ("MySQL",2);
 INSERT INTO TestTable VALUES ("Now",33);
 
-GRANT SELECT,INSERT,UPDATE,DELETE
-ON ##database##.TestTable
-TO '##username##'@'localhost' IDENTIFIED BY '##password##';
-
-GRANT SELECT,INSERT,UPDATE,DELETE
-ON ##database##.TestTable
-TO '##username##'@'##server##' IDENTIFIED BY '##password##';
-
