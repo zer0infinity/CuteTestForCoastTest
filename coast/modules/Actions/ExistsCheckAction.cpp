a323c79f5fcb829cadda02427b3a6abfb62fd0f5
Marcel Huber <marcel.huber@hsr.ch>
2011-03-21 10:29:19 +0100

    extended ExistsCheckAction to use different path and index delimiters for Context::Lookup when requested
    Note: configuration change required!

--- coast/modules/Actions/ExistsCheckAction.cpp
+++ coast/modules/Actions/ExistsCheckAction.cpp
@@ -5,35 +5,53 @@
  * This library/application is free software; you can redistribute and/or modify it under the terms of
  * the license that is included with this library/application in the file license.txt.
  */
-
-//--- interface include --------------------------------------------------------
 #include "ExistsCheckAction.h"
-
-//--- project modules used -----------------------------------------------------
-
-//--- standard modules used ----------------------------------------------------
 #include "Context.h"
 #include "Renderer.h"
 #include "Dbg.h"
-
-//--- c-modules used -----------------------------------------------------------
-
 //---- ExistsCheckAction ---------------------------------------------------------------
 RegisterAction(ExistsCheckAction);
 
-ExistsCheckAction::ExistsCheckAction(const char *name) : Action(name) { }
+namespace {
+	const char *gcSlotName = "LookupName";
+	const char *gcDelimName = "Delim";
+	const char *gcIndexDelimName = "IndexDelim";
+	long const lookupNameIndex = 0L,
+			delimIndex = 1L,
+			indexDelimIndex = 2L;
+}
 
-ExistsCheckAction::~ExistsCheckAction() { }
-
-bool ExistsCheckAction::DoExecAction(String &transitionToken, Context &ctx, const ROAnything &config)
-{
+bool ExistsCheckAction::DoExecAction(String &transitionToken, Context &ctx, const ROAnything &config) {
 	StartTrace(ExistsCheckAction.DoExecAction);
 
-	String path;
-	Renderer::RenderOnString(path, ctx, config);
-	ROAnything collector;
+	bool isSimpleArray = false;
 
-	bool ret = ctx.Lookup(path, collector);
-	Trace(path << " exists: " << (ret ? "true" : "false"));
+	ROAnything lookupName;
+	if (!config.LookupPath(lookupName, gcSlotName, '\000')) {
+		isSimpleArray = true;
+		lookupName = config[lookupNameIndex];
+	}
+
+	ROAnything delim;
+	if (!config.LookupPath(delim, gcDelimName, '\000')) {
+		if ( isSimpleArray && config.GetSize() > delimIndex ) {
+			delim = config[delimIndex];
+		}
+	}
+
+	ROAnything indexdelim;
+	if (!config.LookupPath(indexdelim, gcIndexDelimName, '\000')) {
+		if ( isSimpleArray && config.GetSize() > indexDelimIndex ) {
+			indexdelim = config[indexDelimIndex];
+		}
+	}
+
+	String lookup;
+	Renderer::RenderOnString(lookup, ctx, lookupName);
+	Trace("looking up: " << lookup);
+
+	bool ret = ctx.Lookup(lookup, lookupName, delim.AsCharPtr(".")[0L], indexdelim.AsCharPtr(":")[0L]);
+
+	Trace(lookup << " exists: " << (ret ? "true" : "false"));
 	return ret;
 }
--- coast/modules/Actions/ExistsCheckAction.h
+++ coast/modules/Actions/ExistsCheckAction.h
@@ -13,43 +13,75 @@
 #include "Action.h"
 
 //---- ExistsCheckAction ----------------------------------------------------------
-//! Checks whether a given Slot exists.
+//! Checks whether a given slot exists in the Context and return true. False otherwise.
 /*!
-\par Configuration
+ * @section ecac Action configuration
 \code
 {
-	A render spec
+	/LookupName		Rendererspec
+	/Delim   		String
+	/IndexDelim		String
 }
 \endcode
-\par Example:
+or just
 \code
 {
-	"LostAndFound.HandBags."
-	{ /ContextLookupRenderer "Company.Employees.Fred.WifeName" }
-	".Contents"
+	"name" "delim" "indexdelim"
 }
 \endcode
-
-Renders its config onto a string and checks whether the rendered path/slot
-exists in context (e.g. the example checks, whether
-"LostAndFound.Handbags.Cindy.Contents" exists, provided that
-"Company.Employees.Fred.WifeName" is rendered as "Cindy").
-The action returns true if the rendered slot exists, false otherwise.
+ * @par \c LookupName or first index entry
+ * Mandatory\n
+ * Render the given specification into a String conforming to Anything::LookupPath syntax
+ *
+ * @par \c Delim
+ * \b optional, default "."\n
+ * First character is taken as delimiter for named slots
+ *
+ * @par \c IndexDelim
+ * \b optional, default ":"\n
+ * First character is taken as delimiter for indexed slots
+ *
+ * @par Example1:
+ * @code
+{
+	{
+		"LostAndFound.HandBags."
+		{ /ContextLookupRenderer "Company.Employees.Fred.WifeName" }
+		".Contents"
+	}
+}
+ * @endcode
+ * Renders its config into a string and checks whether the rendered path/slot exists in context (e.g. the example checks, whether
+ * \em "LostAndFound.Handbags.Cindy.Contents" exists, provided that \em "Company.Employees.Fred.WifeName" is rendered as "Cindy").
+ * The action returns true if the rendered slot exists, false otherwise.
+ *
+ * @par Example2:
+ * @code
+{
+	/LookupName {
+		"NeedToTestForASlot_ContainingADefault_Delimiter"
+		".Contents"
+	}
+	/Delim "_"
+}
+ * @endcode
+ * In this example, the LookupPath delimiter will be temporarily changed to \b _ so that \c ".Contents" will be treated a regular string without
+ * having the dot being used as a path delimiter.
 */
-class ExistsCheckAction : public Action
-{
+class ExistsCheckAction: public Action {
 public:
-	//--- constructors
 	/*! \param name defines the name of the action */
-	ExistsCheckAction(const char *name);
-	~ExistsCheckAction();
+	ExistsCheckAction(const char *name) :
+		Action(name) {
+	}
 
+protected:
 	/*! Checks whether the slot defined in config exists
-		\param transitionToken (in/out) the event passed by the caller, can be modified.
-		\param ctx the context the action runs within.
-		\param config the configuration of the action (see general descr)
-		\return true if the action run successfully, false if an error occurred.
-	*/
+	 \param transitionToken (in/out) the event passed by the caller, can be modified.
+	 \param ctx the context the action runs within.
+	 \param config the configuration of the action (see general descr)
+	 \return true if the action run successfully, false if an error occurred.
+	 */
 	virtual bool DoExecAction(String &transitionToken, Context &ctx, const ROAnything &config);
 };
 
