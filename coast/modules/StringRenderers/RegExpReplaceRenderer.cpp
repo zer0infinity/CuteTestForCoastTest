73e306161f101aef3d4c09db6e7c5da78bd44901
Marcel Huber <marcel.huber@hsr.ch>
2011-03-29 17:57:02 +0200

    refactored renderer; it is now partly equal to RegExpSearchReplaceResultMapper
    TODO: factor out the similar 'core' parts into a separate file usable by both Renderer and Mapper...

--- coast/modules/StringRenderers/RegExpReplaceRenderer.cpp
+++ coast/modules/StringRenderers/RegExpReplaceRenderer.cpp
@@ -12,47 +12,60 @@
 RegisterRenderer(RegExpReplaceRenderer);
 
 namespace {
+	const char * _Text = "Text";
+	const char * _Expressions = "Expressions";
+	const char * _Pattern = "Pattern";
+	const char * _SimplePattern = "SimplePattern";
+	const char * _Replacement = "Replacement";
+	const char * _MatchFlags = "MatchFlags";
+	const char * _ReplaceAll = "ReplaceAll";
+
 	String getSimpleOrRenderedString(Context &ctx, ROAnything roaConfig) {
 		if (roaConfig.GetType() == AnyArrayType) {
 			return Renderer::RenderToString(ctx, roaConfig);
 		}
 		return roaConfig.AsString();
 	}
+	void searchReplaceExpressions(Context &ctx, String &sText, ROAnything roaExpressions) {
+		StartTrace(RegExpSearchReplaceResultMapper.searchReplaceExpressions);
+		TraceAny(roaExpressions, "list of expressions");
+		// check if we have multiple replacements by testing if first entry in Expressions is an AnyArrayType or not
+		bool singleExpression = not (roaExpressions[0L].GetType() == AnyArrayType);
+		AnyExtensions::Iterator<ROAnything> expressionIterator(singleExpression ? ROAnything() : roaExpressions);
+		ROAnything roaEntry = roaExpressions;
+		if (not singleExpression) {
+			expressionIterator.Next(roaEntry);
+		}
+		TraceAny(roaEntry, (singleExpression?"single ":"prefetched ") << "expression configuration" );
+		do {
+			String sPattern;
+			ROAnything roaPattern, roaMatchFlags;
+			bool isSimplePattern = false;
+			if (roaEntry.LookupPath(roaPattern, _Pattern, '\000') || (isSimplePattern = roaEntry.LookupPath(roaPattern, _SimplePattern,
+					'\000'))) {
+				sPattern = getSimpleOrRenderedString(ctx, roaPattern);
+				if (isSimplePattern) {
+					sPattern = RE::SimplePatternToFullRegularExpression(sPattern);
+				}
+			}
+			String sReplacement = getSimpleOrRenderedString(ctx, roaEntry[_Replacement]);
+			RE aRE(sPattern, static_cast<RE::eMatchFlags> (roaEntry[_MatchFlags].AsLong(0L)));
+			Trace("String [" << sText << "], applying pattern [" << sPattern << "]");
+			sText = aRE.Subst(sText, sReplacement, roaEntry[_ReplaceAll].AsBool(true));
+		} while (expressionIterator.Next(roaEntry));
+		Trace("replaced text [" << sText << "]");
+	}
 }
 
 void RegExpReplaceRenderer::RenderAll(std::ostream &reply, Context &ctx, const ROAnything &config) {
 	StartTrace(RegExpReplaceRenderer.RenderAll);
 	ROAnything roaText, roaExpressions;
-	if (!config.LookupPath(roaText, "Text", '\000') || !config.LookupPath(roaExpressions, "Expressions", '\000')) {
+	if (!config.LookupPath(roaText, _Text, '\000') || !config.LookupPath(roaExpressions, _Expressions, '\000')) {
 		return;
 	}
-	TraceAny(roaExpressions, "list of expressions");
 	// check if the string is already a string value or if it equals a "/Lookup ...", in which case has to be rendered to a string
 	String sText = getSimpleOrRenderedString(ctx, roaText);
-	// check if we have multiple replacements by testing if first entry in Expressions is an AnyArrayType or not
-	bool singleExpression = not (roaExpressions[0L].GetType() == AnyArrayType);
-	AnyExtensions::Iterator<ROAnything> expressionIterator(singleExpression ? ROAnything() : roaExpressions);
-	ROAnything roaEntry = roaExpressions;
-	if (not singleExpression) {
-		expressionIterator.Next(roaEntry);
-	}
-	TraceAny(roaEntry, (singleExpression?"single ":"prefetched ") << "expression configuration" );
-	do {
-		String sPattern;
-		ROAnything roaPattern, roaMatchFlags;
-		bool isSimplePattern = false;
-		if (roaEntry.LookupPath(roaPattern, "Pattern", '\000') || (isSimplePattern = roaEntry.LookupPath(roaPattern, "SimplePattern",
-				'\000'))) {
-			sPattern = getSimpleOrRenderedString(ctx, roaPattern);
-			if (isSimplePattern) {
-				sPattern = RE::SimplePatternToFullRegularExpression(sPattern);
-			}
-		}
-		String sReplacement = getSimpleOrRenderedString(ctx, roaEntry["Replacement"]);
-		RE aRE(sPattern, static_cast<RE::eMatchFlags> (roaEntry["MatchFlags"].AsLong(0L)));
-		Trace("String [" << sText << "], applying pattern [" << sPattern << "]");
-		sText = aRE.Subst(sText, sReplacement, roaEntry["ReplaceAll"].AsBool(true));
-	} while (expressionIterator.Next(roaEntry));
+	searchReplaceExpressions(ctx, sText, roaExpressions);
 	Trace("returning [" << sText << "]");
 	reply << sText;
 }
