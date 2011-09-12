510d7a7e2bec43a51b6fc56730a13d728128d122
Marcel Huber <marcel.huber@hsr.ch>
2011-09-12 15:42:24 +0200

    changed static only class to namespace
    added Flags enumeration type and formatted comments

--- coast/modules/NTLMAuth/NTLMCoder.cpp
+++ coast/modules/NTLMAuth/NTLMCoder.cpp
@@ -11,272 +11,276 @@
 #include <openssl/des.h> // be careful to really use our 3rdparty openssl instead of an older one
 #include <openssl/md4.h>
 
-static long decodeshort(const String &dm, long pos) {
-	return (unsigned char) dm[pos] + ((unsigned char) dm[pos + 1L] << 8);
-}
-static void appendshort(String &buff, unsigned short val) {
-	buff.Append((char) (val & 0xff)).Append((char) (val >> 8));
-}
-static void appendlong(String &buff, long val) {
-	appendshort(buff, val & 0xffff);
-	appendshort(buff, (val >> 16) & 0xffff);
-}
-String NTLMCoder::MakeBase64(const String &buff) {
-	StartTrace(NTLMCoder.MakeBase64);
-	String result;
-	Base64Regular("NTLMb64").DoEncode(result, buff); //not an url
-	Trace("result = " << result);
-	return result;
+namespace NTLMCoder {
+	long decodeshort(const String &dm, long pos) {
+		return (unsigned char) dm[pos] + ((unsigned char) dm[pos + 1L] << 8);
+	}
+	void appendshort(String &buff, unsigned short val) {
+		buff.Append((char) (val & 0xff)).Append((char) (val >> 8));
+	}
+	void appendlong(String &buff, long val) {
+		appendshort(buff, val & 0xffff);
+		appendshort(buff, (val >> 16) & 0xffff);
+	}
+	String MakeBase64(const String &buff) {
+		StartTrace(NTLMCoder.MakeBase64);
+		String result;
+		Base64Regular("NTLMb64").DoEncode(result, buff); //not an url
+		Trace("result = " << result);
+		return result;
 
-}
-bool NTLMCoder::DecodeBase64(String &result, const String &input) {
-	Base64Regular b64("NTLMb64");
-	return b64.DoDecode(result, input);
-}
-
-bool NTLMCoder::DecodeClientMsg(const String &base64msg, String &domain, String &host) {
-	StartTrace(NTLMCoder.DecodeClientMsg);
-	String dm;
-	if (DecodeBase64(dm, base64msg) && "NTLMSSP" == dm.SubString(0L, 7L) && '\x01' == dm[8L] && '\x03' == dm[12L] && '\xb2' == dm[13L]) {
-		Trace("decoded string len: " << dm.Length());
-		long dlen = decodeshort(dm, 16L);
-		long doff = decodeshort(dm, 20L);
-		long hlen = decodeshort(dm, 24L);
-		long hoff = decodeshort(dm, 28L);
-		Trace("doff = " << doff << " dlen = " << dlen << " hoff (32) = " << hoff << " hlen= " << hlen);
-		domain = dm.SubString(doff, dlen);
-		host = dm.SubString(hoff, hlen);
-		return true;
+	}
+	bool DecodeBase64(String &result, const String &input) {
+		Base64Regular b64("NTLMb64");
+		return b64.DoDecode(result, input);
 	}
 
-	return false;
-}
-String NTLMCoder::EncodeClientMsg(String domain, String host) {
-	StartTrace(NTLMCoder.EncodeClientMsg);
-	String buff((void *) "NTLMSSP\0\x01\0\0\0\x03\xb2\0\0", 16L);
-	unsigned short dlen = domain.Length();
-	unsigned short hlen = host.Length();
-	unsigned short doff = 32 + hlen;
-	Trace("doff = " << long(doff) << " dlen = " << long(dlen) << " hlen= " << long(hlen));
-	appendshort(buff, dlen);
-	appendshort(buff, dlen);
-	appendshort(buff, doff);
-	appendshort(buff, 0);
-	appendshort(buff, hlen);
-	appendshort(buff, hlen);
-	appendshort(buff, 0x20);
-	appendshort(buff, 0);
-	Trace("should be 32:" << buff.Length());
-	Assert(32 == buff.Length());
-	host.ToUpper();
-	domain.ToUpper();
-	Trace("host.ToUpper() = " << host << " domain.ToUpper() = " << domain);
-	buff.Append(host);
-	buff.Append(domain);
-	Trace(buff.Length());
-	Assert(buff.Length() == (32 + hlen + dlen));
-	return MakeBase64(buff);
-}
+	bool DecodeClientMsg(const String &base64msg, String &domain, String &host) {
+		StartTrace(NTLMCoder.DecodeClientMsg);
+		String dm;
+		if (DecodeBase64(dm, base64msg) && "NTLMSSP" == dm.SubString(0L, 7L) && '\x01' == dm[8L] && '\x03' == dm[12L]
+				&& '\xb2' == dm[13L]) {
+			Trace("decoded string len: " << dm.Length());
+			long dlen = decodeshort(dm, 16L);
+			long doff = decodeshort(dm, 20L);
+			long hlen = decodeshort(dm, 24L);
+			long hoff = decodeshort(dm, 28L);
+			Trace("doff = " << doff << " dlen = " << dlen << " hoff (32) = " << hoff << " hlen= " << hlen);
+			domain = dm.SubString(doff, dlen);
+			host = dm.SubString(hoff, hlen);
+			return true;
+		}
 
-String NTLMCoder::EncodeServerNonce(String nonce, long flags) {
-	StartTrace(NTLMCoder.EncodeServerNonce);
-	String buff((void *) "NTLMSSP\0\x2\0\0\0\x0\0\0\0\x28\0\0\0", 20L);
-	appendlong(buff, flags);
-	long i;
-	for (i = 0L; i < 8; i++) {
-		buff.Append(nonce[i]);
+		return false;
 	}
-	for (i = 0; i < 8; i++) {
-		buff.Append((char) 0);
-	} Assert(40L == buff.Length());
-	return MakeBase64(buff);
-}
-
-bool NTLMCoder::DecodeServerNonce(const String &base64msg, String &nonce) {
-	StartTrace(NTLMCoder.DecodeServerNonce);
-	String dm;
-	if (DecodeBase64(dm, base64msg) && 40L == dm.Length() && "NTLMSSP" == dm.SubString(0L, 7L) && '\x02' == dm[8L] && '\x28' == dm[16L]
-			&& '\0' == dm[17L] && '\x01' == dm[20L] && '\x82' == dm[21L]) {
-		nonce = dm.SubString(24L, 8L);
-		Trace("nonce = " << nonce);
-		// should be hex?
-		return true;
-	}
-	return false;
-}
-
-/*
- * turns a 56 bit key into the 64 bit, odd parity key and sets the key.
- * The key schedule ks is also set.
- */
-static void setup_des_key(const String &keystr, DES_key_schedule *ks) {
-	DES_cblock key;
-	const unsigned char *key_56 = (const unsigned char *) (const char *) keystr;
-
-	key[0] = key_56[0];
-	key[1] = ((key_56[0] << 7) & 0xFF) | (key_56[1] >> 1);
-	key[2] = ((key_56[1] << 6) & 0xFF) | (key_56[2] >> 2);
-	key[3] = ((key_56[2] << 5) & 0xFF) | (key_56[3] >> 3);
-	key[4] = ((key_56[3] << 4) & 0xFF) | (key_56[4] >> 4);
-	key[5] = ((key_56[4] << 3) & 0xFF) | (key_56[5] >> 5);
-	key[6] = ((key_56[5] << 2) & 0xFF) | (key_56[6] >> 6);
-	key[7] = (key_56[6] << 1) & 0xFF;
-
-	DES_set_odd_parity(&key);
-	DES_set_key(&key, ks);
-}
-
-/*
- * takes a 21 byte array and treats it as 3 56-bit DES keys. The
- * 8 byte plaintext is encrypted with each key and the resulting 24
- * bytes are stored in the results array.
- */
-String NTLMCoder::CalculateNonceHash(const String &keys, const String &plaintext) {
-	DES_key_schedule ks;
-	DES_cblock pt8;
-	for (long i = 0; i < 8; i++) {
-		pt8[i] = (unsigned char) plaintext[i];
+	String EncodeClientMsg(String domain, String host) {
+		StartTrace(NTLMCoder.EncodeClientMsg);
+		String buff((void *) "NTLMSSP\0\x01\0\0\0\x03\xb2\0\0", 16L);
+		unsigned short dlen = domain.Length();
+		unsigned short hlen = host.Length();
+		unsigned short doff = 32 + hlen;
+		Trace("doff = " << long(doff) << " dlen = " << long(dlen) << " hlen= " << long(hlen));
+		appendshort(buff, dlen);
+		appendshort(buff, dlen);
+		appendshort(buff, doff);
+		appendshort(buff, 0);
+		appendshort(buff, hlen);
+		appendshort(buff, hlen);
+		appendshort(buff, 0x20);
+		appendshort(buff, 0);
+		Trace("should be 32:" << buff.Length());
+		Assert(32 == buff.Length());
+		host.ToUpper();
+		domain.ToUpper();
+		Trace("host.ToUpper() = " << host << " domain.ToUpper() = " << domain);
+		buff.Append(host);
+		buff.Append(domain);
+		Trace(buff.Length());
+		Assert(buff.Length() == (32 + hlen + dlen));
+		return MakeBase64(buff);
 	}
 
-	String result;
-	DES_cblock res;
-	setup_des_key(keys.SubString(0L, 7L), &ks);
-	DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
-	result.Append((void *) &res[0], 8);
-
-	setup_des_key(keys.SubString(7L, 7L), &ks);
-	DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
-	result.Append((void *) &res[0], 8);
-
-	setup_des_key(keys.SubString(14L, 7L), &ks);
-	DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
-	result.Append((void *) &res[0], 8);
-	return result;
-}
-
-String NTLMCoder::EncodeLMPassword(String password) {
-	StartTrace(NTLMCoder.EncodeLMPassword);
-	Trace("password: '" << password << "'");
-	password.Trim(14L);
-	password.ToUpper();
-	while (password.Length() < 14) {
-		password.Append(char(0));
-	}Trace("password as used:" << password);
-	/* create LanManager hashed password */
-
-	unsigned char magic[8] = { 0x4B, 0x47, 0x53, 0x21, 0x40, 0x23, 0x24, 0x25 };
-	Trace("magic = " << String((const char *)magic, 8));
-	DES_cblock lmhpw;
-	Assert(8 == sizeof(lmhpw));
-	des_key_schedule ks;
-	String result;
-	setup_des_key(password.SubString(0L, 7L), &ks);
-	DES_ecb_encrypt(&magic, &lmhpw, &ks, DES_ENCRYPT);
-	long i;
-	for (i = 0; i < 8; i++) {
-		result.Append((char) lmhpw[i]);
+	String EncodeServerNonce(String nonce, long flags) {
+		StartTrace(NTLMCoder.EncodeServerNonce);
+		String buff((void *) "NTLMSSP\0\x2\0\0\0\x0\0\0\0\x28\0\0\0", 20L);
+		appendlong(buff, flags);
+		long i;
+		for (i = 0L; i < 8; i++) {
+			buff.Append(nonce[i]);
+		}
+		for (i = 0; i < 8; i++) {
+			buff.Append((char) 0);
+		}Assert(40L == buff.Length());
+		return MakeBase64(buff);
 	}
 
-	setup_des_key(password.SubString(7L, 7L), &ks);
-	DES_ecb_encrypt(&magic, &lmhpw, &ks, DES_ENCRYPT);
-	for (i = 0; i < 8; i++) {
-		result.Append((char) lmhpw[i]);
+	bool DecodeServerNonce(const String &base64msg, String &nonce) {
+		StartTrace(NTLMCoder.DecodeServerNonce);
+		String dm;
+		if (DecodeBase64(dm, base64msg) && 40L == dm.Length() && "NTLMSSP" == dm.SubString(0L, 7L) && '\x02' == dm[8L] && '\x28' == dm[16L]
+				&& '\0' == dm[17L] && '\x01' == dm[20L] && '\x82' == dm[21L]) {
+			nonce = dm.SubString(24L, 8L);
+			Trace("nonce = " << nonce);
+			// should be hex?
+			return true;
+		}
+		return false;
 	}
-	while (result.Length() < 21) {
-		result.Append(char(0));
-	} Assert(result.Length() == 21);
-	return result;
-}
-String NTLMCoder::EncodeNTPassword(String password) {
-	StartTrace(NTLMCoder.EncodeNTPassword);
-	Trace("password ='" << password << "'");
 
-	String ntpw = MakeUtf16(password);
+	/*
+	 * turns a 56 bit key into the 64 bit, odd parity key and sets the key.
+	 * The key schedule ks is also set.
+	 */
+	void setup_des_key(const String &keystr, DES_key_schedule *ks) {
+		DES_cblock key;
+		const unsigned char *key_56 = (const unsigned char *) (const char *) keystr;
 
-	unsigned char nt_hpw[21];
-	MD4_CTX context;
-	MD4_Init(&context);
-	MD4_Update(&context, (const char *) ntpw, ntpw.Length());
-	MD4_Final(nt_hpw, &context);
-	String result((void *) nt_hpw, 16L);
-	while (result.Length() < 21) {
-		result.Append(char(0));
-	} Assert(result.Length() == 21);
-	return result;
-}
-String NTLMCoder::MakeUtf16(const String &input) {
-	String result;
-	for (long i = 0; i < input.Length(); i++) {
-		result.Append(input[i]);
-		result.Append((char) '\0'); // "dummy" UTF 16 encoding
+		key[0] = key_56[0];
+		key[1] = ((key_56[0] << 7) & 0xFF) | (key_56[1] >> 1);
+		key[2] = ((key_56[1] << 6) & 0xFF) | (key_56[2] >> 2);
+		key[3] = ((key_56[2] << 5) & 0xFF) | (key_56[3] >> 3);
+		key[4] = ((key_56[3] << 4) & 0xFF) | (key_56[4] >> 4);
+		key[5] = ((key_56[4] << 3) & 0xFF) | (key_56[5] >> 5);
+		key[6] = ((key_56[5] << 2) & 0xFF) | (key_56[6] >> 6);
+		key[7] = (key_56[6] << 1) & 0xFF;
+
+		DES_set_odd_parity(&key);
+		DES_set_key(&key, ks);
 	}
-	return result;
 
-}
-static String cutfrommessage(const String dm, long lengthpos) {
-	return dm.SubString(decodeshort(dm, lengthpos + 4), decodeshort(dm, lengthpos));
-}
-String NTLMCoder::StripUtf16(const String &input) {
-	String result(input.Length() / 2);
-	for (long i = 0; i < input.Length(); i += 2) {
-		result.Append(input[i]);
-		Assert(input[i+1L] == 0);
-	}
-	return result;
-}
-bool NTLMCoder::DecodeResponse(const String &base64msg, const String &nonce, const String &lmhash, const String &nthash, String &domain,
-		String &host, String &user) {
-	StartTrace(NTLMCoder.DecodeResponse);
-	String dm;
-	if (DecodeBase64(dm, base64msg) && "NTLMSSP" == dm.SubString(0L, 7L) && '\x03' == dm[8L] && '\x01' == dm[60L] && '\x82' == dm[61L]
-			&& dm.Length() == decodeshort(dm, 56L)) {
-		Trace("decoded string len: " << dm.Length());
-		domain = StripUtf16(cutfrommessage(dm, 28L));
-		user = StripUtf16(cutfrommessage(dm, 36L));
-		host = StripUtf16(cutfrommessage(dm, 44L));
-		Trace("domain =" << domain << " user =" << user << " host =" << host);
-		return CalculateNonceHash(lmhash, nonce) == cutfrommessage(dm, 12L) && CalculateNonceHash(nthash, nonce) == cutfrommessage(dm, 20L);
-	}
-	return false;
-}
-static void appendlenoff(String &buff, long length, short off) {
-	appendshort(buff, length);
-	appendshort(buff, length);
-	appendshort(buff, off);
-	appendshort(buff, 0);
-}
-String NTLMCoder::MakeUtf16Upper(const String &in) {
-	String s = in;
-	s.ToUpper();
-	return MakeUtf16(s);
-}
-String NTLMCoder::EncodeResponse(const String &nonce, const String &lmhash, const String &nthash, const String &domain, const String &host,
-		const String &user) {
-	StartTrace(NTLMCoder.EncodeResponse);
-	String buff((void *) "NTLMSSP\0\x03\0\0\0", 12L);
-	String lmresp = CalculateNonceHash(lmhash, nonce);
-	String ntresp = CalculateNonceHash(nthash, nonce);
-	String utfdom = MakeUtf16Upper(domain);
-	String utfhost = MakeUtf16Upper(host);
-	String utfuser = MakeUtf16(user);
-	short uoff = 64 + utfdom.Length();
-	short hoff = uoff + utfuser.Length();
-	short lmoff = hoff + utfhost.Length();
-	short msglen = lmoff + 24 + 24;
-	appendlenoff(buff, lmresp.Length(), lmoff);
-	appendlenoff(buff, ntresp.Length(), lmoff + lmresp.Length());
-	appendlenoff(buff, utfdom.Length(), 64);
-	appendlenoff(buff, utfuser.Length(), uoff);
-	appendlenoff(buff, utfhost.Length(), hoff);
-	appendlenoff(buff, 0, msglen);
-	//--- 52 ---
-	appendshort(buff, 0x8201);
-	appendshort(buff, 0);
-	Assert(64 == buff.Length());
-	buff.Append(utfdom).Append(utfuser).Append(utfhost);
-	buff.Append(lmresp).Append(ntresp);
-	Assert(msglen == buff.Length());
-	//--- done with message
-	return MakeBase64(buff);
+	/*
+	 * takes a 21 byte array and treats it as 3 56-bit DES keys. The
+	 * 8 byte plaintext is encrypted with each key and the resulting 24
+	 * bytes are stored in the results array.
+	 */
+	String CalculateNonceHash(const String &keys, const String &plaintext) {
+		DES_key_schedule ks;
+		DES_cblock pt8;
+		for (long i = 0; i < 8; i++) {
+			pt8[i] = (unsigned char) plaintext[i];
+		}
 
+		String result;
+		DES_cblock res;
+		setup_des_key(keys.SubString(0L, 7L), &ks);
+		DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
+		result.Append((void *) &res[0], 8);
+
+		setup_des_key(keys.SubString(7L, 7L), &ks);
+		DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
+		result.Append((void *) &res[0], 8);
+
+		setup_des_key(keys.SubString(14L, 7L), &ks);
+		DES_ecb_encrypt(&pt8, &res, &ks, DES_ENCRYPT);
+		result.Append((void *) &res[0], 8);
+		return result;
+	}
+
+	String EncodeLMPassword(String password) {
+		StartTrace(NTLMCoder.EncodeLMPassword);
+		Trace("password: '" << password << "'");
+		password.Trim(14L);
+		password.ToUpper();
+		while (password.Length() < 14) {
+			password.Append(char(0));
+		}Trace("password as used:" << password);
+		/* create LanManager hashed password */
+
+		unsigned char magic[8] = { 0x4B, 0x47, 0x53, 0x21, 0x40, 0x23, 0x24, 0x25 };
+		Trace("magic = " << String((const char *)magic, 8));
+		DES_cblock lmhpw;
+		Assert(8 == sizeof(lmhpw));
+		des_key_schedule ks;
+		String result;
+		setup_des_key(password.SubString(0L, 7L), &ks);
+		DES_ecb_encrypt(&magic, &lmhpw, &ks, DES_ENCRYPT);
+		long i;
+		for (i = 0; i < 8; i++) {
+			result.Append((char) lmhpw[i]);
+		}
+
+		setup_des_key(password.SubString(7L, 7L), &ks);
+		DES_ecb_encrypt(&magic, &lmhpw, &ks, DES_ENCRYPT);
+		for (i = 0; i < 8; i++) {
+			result.Append((char) lmhpw[i]);
+		}
+		while (result.Length() < 21) {
+			result.Append(char(0));
+		}Assert(result.Length() == 21);
+		return result;
+	}
+	String EncodeNTPassword(String password) {
+		StartTrace(NTLMCoder.EncodeNTPassword);
+		Trace("password ='" << password << "'");
+
+		String ntpw = MakeUtf16(password);
+
+		unsigned char nt_hpw[21];
+		MD4_CTX context;
+		MD4_Init(&context);
+		MD4_Update(&context, (const char *) ntpw, ntpw.Length());
+		MD4_Final(nt_hpw, &context);
+		String result((void *) nt_hpw, 16L);
+		while (result.Length() < 21) {
+			result.Append(char(0));
+		}Assert(result.Length() == 21);
+		return result;
+	}
+	String MakeUtf16(const String &input) {
+		String result;
+		for (long i = 0; i < input.Length(); i++) {
+			result.Append(input[i]);
+			result.Append((char) '\0'); // "dummy" UTF 16 encoding
+		}
+		return result;
+
+	}
+	String cutfrommessage(const String dm, long lengthpos) {
+		return dm.SubString(decodeshort(dm, lengthpos + 4), decodeshort(dm, lengthpos));
+	}
+	String StripUtf16(const String &input) {
+		String result(input.Length() / 2);
+		for (long i = 0; i < input.Length(); i += 2) {
+			result.Append(input[i]);
+			Assert(input[i+1L] == 0);
+		}
+		return result;
+	}
+	bool DecodeResponse(const String &base64msg, const String &nonce, const String &lmhash, const String &nthash, String &domain,
+			String &host, String &user) {
+		StartTrace(NTLMCoder.DecodeResponse);
+		String dm;
+		if (DecodeBase64(dm, base64msg) && "NTLMSSP" == dm.SubString(0L, 7L) && '\x03' == dm[8L] && '\x01' == dm[60L] && '\x82' == dm[61L]
+				&& dm.Length() == decodeshort(dm, 56L)) {
+			Trace("decoded string len: " << dm.Length());
+			domain = StripUtf16(cutfrommessage(dm, 28L));
+			user = StripUtf16(cutfrommessage(dm, 36L));
+			host = StripUtf16(cutfrommessage(dm, 44L));
+			Trace("domain =" << domain << " user =" << user << " host =" << host);
+			return CalculateNonceHash(lmhash, nonce) == cutfrommessage(dm, 12L)
+					&& CalculateNonceHash(nthash, nonce) == cutfrommessage(dm, 20L);
+		}
+		return false;
+	}
+	void appendlenoff(String &buff, long length, short off) {
+		appendshort(buff, length);
+		appendshort(buff, length);
+		appendshort(buff, off);
+		appendshort(buff, 0);
+	}
+	String MakeUtf16Upper(const String &in) {
+		String s = in;
+		s.ToUpper();
+		return MakeUtf16(s);
+	}
+	String EncodeResponse(const String &nonce, const String &lmhash, const String &nthash, const String &domain, const String &host,
+			const String &user) {
+		StartTrace(NTLMCoder.EncodeResponse);
+		String buff((void *) "NTLMSSP\0\x03\0\0\0", 12L);
+		String lmresp = CalculateNonceHash(lmhash, nonce);
+		String ntresp = CalculateNonceHash(nthash, nonce);
+		String utfdom = MakeUtf16Upper(domain);
+		String utfhost = MakeUtf16Upper(host);
+		String utfuser = MakeUtf16(user);
+		short uoff = 64 + utfdom.Length();
+		short hoff = uoff + utfuser.Length();
+		short lmoff = hoff + utfhost.Length();
+		short msglen = lmoff + 24 + 24;
+		appendlenoff(buff, lmresp.Length(), lmoff);
+		appendlenoff(buff, ntresp.Length(), lmoff + lmresp.Length());
+		appendlenoff(buff, utfdom.Length(), 64);
+		appendlenoff(buff, utfuser.Length(), uoff);
+		appendlenoff(buff, utfhost.Length(), hoff);
+		appendlenoff(buff, 0, msglen);
+		//--- 52 ---
+		appendshort(buff, 0x8201);
+		appendshort(buff, 0);
+		Assert(64 == buff.Length());
+		buff.Append(utfdom).Append(utfuser).Append(utfhost);
+		buff.Append(lmresp).Append(ntresp);
+		Assert(msglen == buff.Length());
+		//--- done with message
+		return MakeBase64(buff);
+
+	}
 }
--- coast/modules/NTLMAuth/NTLMCoder.h
+++ coast/modules/NTLMAuth/NTLMCoder.h
@@ -15,14 +15,105 @@
 /*!
  basic mechanics for implementing WWW-Authenticate: NTLM
  */
-class NTLMCoder {
-	NTLMCoder() {
-	}
-	NTLMCoder(const NTLMCoder &) {
-	}
-	~NTLMCoder() {
-	}
-public:
+namespace NTLMCoder {
+	enum Flags {
+		/*! Indicates that Unicode strings are supported for use in security
+		 * buffer data. */
+		NegotiateUnicode = 0x00000001,
+		/*! Indicates that OEM strings are supported for use in security
+		 * buffer data. */
+		NegotiateOEM = 0x00000002,
+		/*! Requests that the server's authentication realm be included in
+		 * the Type 2 message. */
+		RequestTarget = 0x00000004,
+		/*! This flag's usage has not been identified. */
+		unknown1 = 0x00000008,
+		/*! Specifies that authenticated communication between the client and
+		 * server should carry a digital signature (message integrity). */
+		NegotiateSign = 0x00000010,
+		/*! Specifies that authenticated communication between the client and
+		 * server should be encrypted (message confidentiality). */
+		NegotiateSeal = 0x00000020,
+		/*! Indicates that datagram authentication is being used. */
+		NegotiateDatagramStyle = 0x00000040,
+		/*! Indicates that the Lan Manager Session Key should be used for
+		 * signing and sealing authenticated communications. */
+		NegotiateLanManagerKey = 0x00000080,
+		/*! This flag's usage has not been identified. */
+		NegotiateNetware = 0x00000100,
+		/*! Indicates that NTLM authentication is being used. */
+		NegotiateNTLM = 0x00000200,
+		/*! This flag's usage has not been identified. */
+		unknown2 = 0x00000400,
+		/*! Sent by the client in the Type 3 message to indicate that an
+		 * anonymous context has been established. This also affects the
+		 * response fields (as detailed in the "Anonymous Response" section). */
+		NegotiateAnonymous = 0x00000800,
+		/*! Sent by the client in the Type 1 message to indicate that the
+		 * name of the domain in which the client workstation has membership
+		 * is included in the message. This is used by the server to determine
+		 * whether the client is eligible for local authentication. */
+		NegotiateDomainSupplied = 0x00001000,
+		/*! Sent by the client in the Type 1 message to indicate that the
+		 * client workstation's name is included in the message. This is used
+		 * by the server to determine whether the client is eligible for local
+		 * authentication. */
+		NegotiateWorkstationSupplied = 0x00002000,
+		/*! Sent by the server to indicate that the server and client are on
+		 * the same machine. Implies that the client may use the established
+		 * local credentials for authentication instead of calculating a
+		 * response to the challenge. */
+		NegotiateLocalCall = 0x00004000,
+		/*! Indicates that authenticated communication between the client and
+		 * server should be signed with a "dummy" signature. */
+		NegotiateAlwaysSign = 0x00008000,
+		/*! Sent by the server in the Type 2 message to indicate that the
+		 * target authentication realm is a domain. */
+		TargetTypeDomain = 0x00010000,
+		/*! Sent by the server in the Type 2 message to indicate that the
+		 * target authentication realm is a server. */
+		TargetTypeServer = 0x00020000,
+		/*! Sent by the server in the Type 2 message to indicate that the
+		 * target authentication realm is a share. Presumably, this is for
+		 * share-level authentication. Usage is unclear. */
+		TargetTypeShare = 0x00040000,
+		/*! Indicates that the NTLM2 signing and sealing scheme should be used
+		 *  for protecting authenticated communications. Note that this refers
+		 *  to a particular session security scheme, and is not related to the
+		 *  use of NTLMv2 authentication. This flag can, however, have an
+		 *  effect on the response calculations (as detailed in the
+		 *  "NTLM2 Session Response" section). */
+		NegotiateNTLM2Key = 0x00080000,
+		/*! This flag's usage has not been identified. */
+		RequestInitResponse = 0x00100000,
+		/*! This flag's usage has not been identified. */
+		RequestAcceptResponse = 0x00200000,
+		/*! This flag's usage has not been identified. */
+		RequestNonNTSessionKey = 0x00400000,
+		/*! Sent by the server in the Type 2 message to indicate that it is
+		 * including a Target Information block in the message. The Target
+		 * Information block is used in the calculation of the NTLMv2
+		 * response. */
+		NegotiateTargetInfo = 0x00800000,
+		/*! This flag's usage has not been identified. */
+		unknown3 = 0x01000000,
+		/*! This flag's usage has not been identified. */
+		unknown4 = 0x02000000,
+		/*! This flag's usage has not been identified. */
+		unknown5 = 0x04000000,
+		/*! This flag's usage has not been identified. */
+		unknown6 = 0x08000000,
+		/*! This flag's usage has not been identified. */
+		unknown7 = 0x10000000,
+		/*! Indicates that 128-bit encryption is supported. */
+		Negotiate128 = 0x20000000,
+		/*! Indicates that the client will provide an encrypted master key in
+		 * the "Session Key" field of the Type 3 message. */
+		NegotiateKeyExchange = 0x40000000,
+		/*! Indicates that 56-bit encryption is supported. */
+		Negotiate56 = 0x80000000,
+	};
+
 	//! Decode "Type1" message from a client to server
 	//! assume messages String is base64 encoded as in
 	//! Authorization: NTLM base64encodedresponsetype1containingdomainandhost
@@ -33,24 +124,24 @@ public:
 	//! \param domain return value of decoded domain
 	//! \param host  return value of decoded host name (not FQDN)
 	//! \return if message conforms to format
-	static bool DecodeClientMsg(const String &base64msg, String &domain, String &host);
-	static String EncodeClientMsg(String domain, String host);
-	static String EncodeServerNonce(String nonce, long flags = 0x8201);
-	static bool DecodeServerNonce(const String &base64msg, String &nonce);
-	static bool DecodeResponse(const String &base64msg, const String &nonce, const String &lmhash, const String &nthash, String &domain,
+	bool DecodeClientMsg(const String &base64msg, String &domain, String &host);
+	String EncodeClientMsg(String domain, String host);
+	String EncodeServerNonce(String nonce, long flags = 0x8201);
+	bool DecodeServerNonce(const String &base64msg, String &nonce);
+	bool DecodeResponse(const String &base64msg, const String &nonce, const String &lmhash, const String &nthash, String &domain,
 			String &host, String &user);
-	static String EncodeResponse(const String &nonce, const String &lmhash, const String &nthash, const String &domain, const String &host,
+	String EncodeResponse(const String &nonce, const String &lmhash, const String &nthash, const String &domain, const String &host,
 			const String &user);
 
-	static String EncodeLMPassword(String password);
-	static String EncodeNTPassword(String password);
-	static String MakeUtf16(const String &input);
-	static String StripUtf16(const String &input);
-	static String CalculateNonceHash(const String &keys, const String &plaintext);
+	String EncodeLMPassword(String password);
+	String EncodeNTPassword(String password);
+	String MakeUtf16(const String &input);
+	String StripUtf16(const String &input);
+	String CalculateNonceHash(const String &keys, const String &plaintext);
 
-	static String MakeBase64(const String &buffer);
-	static bool DecodeBase64(String &result, const String &input);
-	static String MakeUtf16Upper(const String &in);
+	String MakeBase64(const String &buffer);
+	bool DecodeBase64(String &result, const String &input);
+	String MakeUtf16Upper(const String &in);
 };
 
 #endif
