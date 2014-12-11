c6b6843ee78a0a8dc681e1c762c7794451e4323e
Marcel Huber <marcelhuberfoo@gmail.com>
2014-12-11 15:21:22 +0100

    added more tests for NullParameterMapper
    Change-Id: I0894312584cf36fda981ef59894165133f595798

--- coast/modules/Oracle/NullParameterMapper.cpp
+++ coast/modules/Oracle/NullParameterMapper.cpp
@@ -12,40 +12,36 @@
 
 RegisterParameterMapper(NullParameterMapper);
 
-NullParameterMapper::NullParameterMapper( const char *name ) :
-	ParameterMapper( name )
-{
+NullParameterMapper::NullParameterMapper(const char *name) :
+		ParameterMapper(name) {
 	StatTrace(NullParameterMapper.Ctor, name, coast::storage::Current());
 }
 
-IFAObject *NullParameterMapper::Clone(Allocator *a) const
-{
+IFAObject *NullParameterMapper::Clone(Allocator *a) const {
 	StatTrace(NullParameterMapper.Clone, fName, coast::storage::Current());
-	return new (a) NullParameterMapper( fName );
+	return new (a) NullParameterMapper(fName);
 }
 
-bool NullParameterMapper::DoGetAny( const char *key, Anything &value, Context &ctx, ROAnything script )
-{
-	StartTrace1(NullParameterMapper.DoGetAny, NotNull(key) );
-	if ( ParameterMapper::DoGetAny( key, value, ctx, script) ) {
+bool NullParameterMapper::DoGetAny(const char *key, Anything &value, Context &ctx, ROAnything script) {
+	StartTrace1(NullParameterMapper.DoGetAny, NotNull(key));
+	if (ParameterMapper::DoGetAny(key, value, ctx, script)) {
 		Trace("value before replacement [" << (value.IsNull()?String("*"):value.AsString()) << "]");
-		ROAnything roaTreatAsNullValues=Lookup("TreatAsNull");
-		if ( !value.IsNull() && !roaTreatAsNullValues.IsNull() ) {
+		ROAnything roaTreatAsNullValues = Lookup("TreatAsNull");
+		if (!value.IsNull() && !roaTreatAsNullValues.IsNull()) {
 			ROAnything roaTestValue;
 			AnyExtensions::Iterator<ROAnything> nullValIterator(roaTreatAsNullValues);
-			while ( nullValIterator.Next(roaTestValue) ) {
+			while (nullValIterator.Next(roaTestValue)) {
 				Trace("testing against listvalue [" << roaTestValue.AsString() << "]");
-				if ( roaTestValue.IsEqual(value) ) {
+				if (roaTestValue.IsEqual(value)) {
 					Trace("value matched in list [" << value.AsString() << "]");
 					value = Anything(value.GetAllocator());
 					break;
 				}
 			}
 		}
-		Trace("returning true");
+		Trace("returning true and value [" << (value.IsNull()?String("*"):value.AsString()) << "]");
 		return true;
 	}
-	Trace("returning false");
+	Trace("returning false and value [" << (value.IsNull()?String("*"):value.AsString()) << "]");
 	return false;
 }
-
